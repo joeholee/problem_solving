be57c08723c98f78575425866a9eafa2a9e00c58
Joeho <84345021+joeholee@users.noreply.github.com>
2024-07-29 06:18:01 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 668 ms, Memory: 24056 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i=1; i<=N-1; i++) {
+            for(int j=1; j<=i; j++) System.out.print("*");
+            for(int k=2*N-1; k>=2*i; k--) System.out.print(" ");
+            for(int j=1; j<=i; j++) System.out.print("*");
+            System.out.println();
+        }
+        for(int i=1; i<=N; i++) {
+            for(int j=N; j>=i; j--) System.out.print("*");
+            for(int k=1; k<=2*(i-1); k++) System.out.print(" ");
+            for(int j=N; j>=i; j--) System.out.print("*");
+            System.out.println();
+        }
+    }
+}
