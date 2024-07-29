d22ce6652cc0183eecfc33aba34fcc2fa47967c2
Joeho <84345021+joeholee@users.noreply.github.com>
2024-07-29 17:55:21 +0900

    [D1] Title: 최대수 구하기, Time: 7 ms, Memory: 13,372 KB -BaekjoonHub

--- SWEA/D1/2068. 최대수 구하기/최대수 구하기.cpp
+++ SWEA/D1/2068. 최대수 구하기/최대수 구하기.cpp
@@ -1,19 +1,17 @@
-import java.util.Scanner;
-import java.util.Arrays;
+#include <bits/stdc++.h>
+using namespace std;
 
-class Solution
+int main(int argc, char** argv)
 {
-	public static void main(String args[]) throws Exception
+	int test_case;
+	int T;
+	cin>>T;
+	for(test_case = 1; test_case <= T; ++test_case)
 	{
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-            int[] arr = new int[10];
-            for(int i=0; i<10; i++) arr[i]=sc.nextInt();
-            Arrays.sort(arr);
-            System.out.println("#"+test_case+" "+arr[9]);
-		}
+        int arr[10];
+        for(int i=0; i<10; i++) cin >> arr[i];
+        sort(arr,arr+10);
+        cout << "#" << test_case << " " << arr[9] << '\n';
 	}
+	return 0;
 }
