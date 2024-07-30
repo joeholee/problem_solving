8075375afd01930c0c7bdec4fdfe7b4e24d7ddcd
Joeho <84345021+joeholee@users.noreply.github.com>
2024-07-30 22:53:17 +0900

    [D1] Title: 중간값 찾기, Time: 145 ms, Memory: 21,296 KB -BaekjoonHub

--- SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
+++ SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
@@ -7,7 +7,15 @@ int main(int argc, char** argv)
     cin >> N;
     int arr[N];
     for(int i=0; i<N; i++) cin >> arr[i];
-    sort(arr,arr+N);
+    for(int i=N-1; i>0; i--) {
+        for(int j=0; j<i; j++) {
+            if(arr[j]>arr[j+1]) {
+                int tmp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=tmp;
+            }
+        }
+    }
     cout << arr[N/2];
 	return 0;
 }
