712de0db8c854770044469fb523adced0030eb89
Joeho <84345021+joeholee@users.noreply.github.com>
2024-07-30 22:45:38 +0900

    [D1] Title: 중간값 찾기, Time: 145 ms, Memory: 21,296 KB -BaekjoonHub

--- SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
+++ SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
@@ -1,32 +1,13 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+#include <bits/stdc++.h>
+using namespace std;
 
-class Solution // bubble sort
+int main(int argc, char** argv)
 {
-	public static void main(String args[]) throws Exception
-	{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = br.readLine();
-        StringTokenizer st = new StringTokenizer(str);
-        int N = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N];
-        str = br.readLine();
-        st = new StringTokenizer(str);
-        for(int i=0; i<N; i++) arr[i] = Integer.parseInt(st.nextToken());
-        bubbleSort(arr);
-        System.out.println(arr[N/2]);
-	}
-    
-    static void bubbleSort(int[] arr) {
-        for(int i=arr.length-1; i>0; i--) {
-            for(int j=0; j<i; j++) {
-                if(arr[j]>arr[j+1]) {
-                    int tmp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = tmp;
-            	}
-       		}
-    	}
-	}
+    int N;
+    cin >> N;
+    int arr[N];
+    for(int i=0; i<N; i++) cin >> arr[i];
+    sort(arr,arr+N);
+    cout << arr[N/2];
+	return 0;
 }
