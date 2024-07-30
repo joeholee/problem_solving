def00662bb08518abf786fea4f0a3dde623d0f5a
Joeho <84345021+joeholee@users.noreply.github.com>
2024-07-30 13:38:50 +0900

    [D1] Title: 중간값 찾기, Time: 145 ms, Memory: 21,296 KB -BaekjoonHub

--- SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
+++ SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
@@ -1,19 +1,32 @@
-import java.util.Scanner;
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class Solution
+class Solution // selection sort
 {
 	public static void main(String args[]) throws Exception
 	{
-		Scanner sc = new Scanner(System.in);
-		int N;
-		N=sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str);
+        int N = Integer.parseInt(st.nextToken());
         int[] arr = new int[N];
-        for(int i=0; i<N; i++) {
-            int tmp = sc.nextInt();
-            arr[i] = tmp;
-        }
-        Arrays.sort(arr);
+        str = br.readLine();
+        st = new StringTokenizer(str);
+        for(int i=0; i<N; i++) arr[i] = Integer.parseInt(st.nextToken());
+        selectionSort(arr);
         System.out.println(arr[N/2]);
 	}
+    
+    static void selectionSort(int[] arr) {
+        for(int i=0; i<arr.length; i++) {
+            int minIdx=i;
+            for(int j=i+1; j<arr.length; j++) {
+                if(arr[j]<arr[minIdx]) minIdx=j;
+            }
+            int tmp=arr[i];
+            arr[i]=arr[minIdx];
+            arr[minIdx]=tmp;
+        }
+    }
 }
