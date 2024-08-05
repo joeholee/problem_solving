ff0c07cc287f2bacdd3dbf682337d8a3fb08fdec
Joeho <84345021+joeholee@users.noreply.github.com>
2024-08-06 06:29:15 +0900

    [Gold V] Title: 달력, Time: 116 ms, Memory: 14516 KB -BaekjoonHub

---
+++ 백준/Gold/20207. 달력/달력.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] cal = new int[366];
+		StringTokenizer st;
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int S = Integer.parseInt(st.nextToken());
+			int E = Integer.parseInt(st.nextToken());
+			for(int j=S; j<=E; j++) cal[j]++;
+		}
+		int ans=0;
+		for(int i=0; i<=365; i++) {
+			if(cal[i]==0) continue;
+			int beg=i;
+			int end=i;
+			int height=cal[i];
+			while(cal[end]!=0) {
+				end++;
+                if(end==366) break;
+				if(cal[end]>height) height=cal[end];
+			}
+			ans+=(end-beg)*height;
+			i+=end-beg;
+		}
+		System.out.print(ans);
+	}
+}
