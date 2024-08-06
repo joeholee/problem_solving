da696aa71fc0a43a3d57466cefd4a7c33e0e0175
Joeho <84345021+joeholee@users.noreply.github.com>
2024-08-07 00:05:35 +0900

    [Silver II] Title: 오리, Time: 128 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Silver/12933. 오리/오리.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.HashMap;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String duck = br.readLine();
+		boolean isValid=true;
+		int ans=0;
+		int[] freq = new int[5];
+		Map<Character,Integer> m = new HashMap<>();
+		m.put('q', 0);
+		m.put('u', 1);
+		m.put('a', 2);
+		m.put('c', 3);
+		m.put('k', 4);
+		out: for(char c : duck.toCharArray()) {
+			if(c=='q') {
+				ans++;
+				if(freq[4]>0) {
+                    ans--;
+                    for(int i=0; i<5; i++) freq[i]--;
+                }
+			}
+			freq[m.get(c)]++;
+			for(int i=0; i<4; i++) {
+				if(freq[i]<freq[i+1]) {
+					isValid=false;
+					break out;
+				}
+			}
+		}
+		for(int i=0; i<4; i++) {
+			if(freq[i]!=freq[i+1]) isValid=false;
+		}
+		System.out.print(isValid? ans : -1);
+	}
+}
