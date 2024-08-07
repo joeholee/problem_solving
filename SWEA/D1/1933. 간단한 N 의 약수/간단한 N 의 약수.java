ce2d7e716569573cef01715f1e22a047073719ad
Joeho <84345021+joeholee@users.noreply.github.com>
2024-08-07 14:54:40 +0900

    [D1] Title: 간단한 N 의 약수, Time: 145 ms, Memory: 20,464 KB -BaekjoonHub

--- SWEA/D1/1933. 간단한 N 의 약수/간단한 N 의 약수.java
+++ SWEA/D1/1933. 간단한 N 의 약수/간단한 N 의 약수.java
@@ -1,19 +1,23 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 
 class Solution
 {
 	public static void main(String args[]) throws Exception
 	{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        boolean[] isDivisor = new boolean[N+1];
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        List<Integer> li = new ArrayList<>();
         for(int i=1; i*i<=N; i++) {
-        	if(N%i==0) {
-            	isDivisor[i]=true;
-                isDivisor[N/i]=true;
+            if(N%i==0) {
+            	if(!li.contains(i)) li.add(i);
+                if(!li.contains(N/i)) li.add(N/i);
             }
         }
-        for(int i=1; i<=N; i++) System.out.print(isDivisor[i] ? i+" " : "");
+        Collections.sort(li);
+        for(int i : li) System.out.print(i+" ");
 	}
 }
