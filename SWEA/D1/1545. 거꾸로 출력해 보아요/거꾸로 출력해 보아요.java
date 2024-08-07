4112c25aa5ee0a460ae326de7a36a8d0440326f8
Joeho <84345021+joeholee@users.noreply.github.com>
2024-08-07 14:57:01 +0900

    [D1] Title: 거꾸로 출력해 보아요, Time: 128 ms, Memory: 20,996 KB -BaekjoonHub

---
+++ SWEA/D1/1545. 거꾸로 출력해 보아요/거꾸로 출력해 보아요.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        while(N>=0) System.out.print(N--+" ");
+	}
+}
