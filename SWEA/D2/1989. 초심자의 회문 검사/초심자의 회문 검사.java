9be012bd9a884eaaba2ac80836b2790f3f4c3855
Joeho <84345021+joeholee@users.noreply.github.com>
2024-08-09 14:49:39 +0900

    [D2] Title: 초심자의 회문 검사, Time: 104 ms, Memory: 18,784 KB -BaekjoonHub

--- SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
@@ -1,26 +1,23 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
- 
-class Solution
-{
-    public static void main(String args[]) throws Exception
-    {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        StringTokenizer st = new StringTokenizer(str);
-        int T = Integer.parseInt(st.nextToken());
-        for(int test_case = 1; test_case <= T; test_case++)
-        {
-            String input = br.readLine();
-            int res=1;
-            for(int i=0; i<input.length()/2; i++) {
-                if(input.charAt(i)!=input.charAt(input.length()-1-i)) {
-                    res=0;
-                    break;
-                }
-            }
-            System.out.println("#"+test_case+" "+res);
-        }
-    }
+
+public class Solution {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		for( int tc = 1; tc <= T ; tc++ ) {
+			String str = br.readLine();
+			
+			int result = 1;
+			for( int i = 0 ; i < str.length()/2 + 1 ; i++) {
+				if(str.charAt(i) != str.charAt(str.length()-i-1)) {
+					result = 0;
+				}
+			}
+		System.out.println( "#" + tc + " " + result );
+		}
+	}
 }
