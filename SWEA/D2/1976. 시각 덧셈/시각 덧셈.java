62281f28caedbaef5a2944ae78cf98317424e8be
Joeho <84345021+joeholee@users.noreply.github.com>
2024-08-14 08:26:44 +0900

    [D2] Title: 시각 덧셈, Time: 161 ms, Memory: 17,156 KB -BaekjoonHub

---
+++ SWEA/D2/1976. 시각 덧셈/시각 덧셈.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+        int T = Integer.parseInt(br.readLine());
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+            sb.setLength(0);
+            String[] time = br.readLine().split(" ");
+            int h1 = Integer.parseInt(time[0]);
+            int m1 = Integer.parseInt(time[1]);
+            int h2 = Integer.parseInt(time[2]);
+            int m2 = Integer.parseInt(time[3]);
+            int ansH=h1+h2;
+            int ansM=m1+m2;
+            if(ansM>=60) {
+            	ansM-=60;
+                ansH+=1;
+            }
+            if(ansH>12) ansH-=12;
+            sb.append("#"+test_case+" "+ansH+" "+ansM);
+            System.out.println(sb);
+		}
+	}
+}
