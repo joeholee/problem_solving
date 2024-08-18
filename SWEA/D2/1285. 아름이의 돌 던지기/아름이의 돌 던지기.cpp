21af69d1926a312055e76c21d7f1b4a6d0372c30
Joeho <84345021+joeholee@users.noreply.github.com>
2024-08-18 19:29:51 +0900

    [D2] Title: 아름이의 돌 던지기, Time: 15 ms, Memory: 13,552 KB -BaekjoonHub

---
+++ SWEA/D2/1285. 아름이의 돌 던지기/아름이의 돌 던지기.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N,arr[1001],near,cnt;
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	cin>>T;
+	for(test_case = 1; test_case <= T; ++test_case)
+	{
+        cin >> N;
+        for(int i=1; i<=N; i++) {
+            cin >> arr[i];
+            arr[i] = abs(arr[i]);
+        }
+        sort(arr+1,arr+N+1);
+        near=arr[1];
+        cnt=1;
+        for(int i=2; i<=N; i++) {
+            if(arr[i]==near) cnt++;
+            else break;
+        }
+        cout << "#" << test_case << " " << near << " " << cnt << "\n";
+	}
+	return 0;
+}
