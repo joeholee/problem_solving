477732f79d8f55f11fcd90a280e18b1047c92226
Joeho <84345021+joeholee@users.noreply.github.com>
2024-08-20 15:59:43 +0900

    [D4] Title: 러시아 국기 같은 깃발, Time: 119 ms, Memory: 18,876 KB -BaekjoonHub

--- SWEA/D4/4613. 러시아 국기 같은 깃발/러시아 국기 같은 깃발.java
+++ SWEA/D4/4613. 러시아 국기 같은 깃발/러시아 국기 같은 깃발.java
@@ -1,45 +1,58 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-            String[] input = br.readLine().split(" ");
-            int N = Integer.parseInt(input[0]);
-            int M = Integer.parseInt(input[1]);
-            char[][] arr = new char[N][M];
-            for(int r=0; r<N; r++) {
-                String tmp = br.readLine();
-            	for(int c=0; c<M; c++) arr[r][c] = tmp.charAt(c);
-            }
-            int ans=2500;
-            int w=0,b=0,r=0;
-            for(int i=0; i<N-2; i++) {
-                w+=countCol(arr,'W',i);
-                b=0;
-                for(int j=i+1; j<N-1;j++) {
-                    b+=countCol(arr,'B',j);
-                    r=0;
-                	for(int k=j+1; k<N; k++) {
-                    	r+=countCol(arr,'R',k);
-                    }
-                    ans=Math.min(ans,w+b+r);
-                }
-            }
-            System.out.println("#"+test_case+" "+ans);
+public class Solution {
+	static int N,M,w,b,r,ans;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static char[][] flag;
+	public static void main(String[] args) throws IOException {
+		int T = Integer.parseInt(br.readLine());
+		for(int tc=1; tc<=T; tc++) {
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			flag = new char[N][M];
+			for(int r=0; r<N; r++) {
+				st = new StringTokenizer(br.readLine());
+				char[] row = st.nextToken().toCharArray();
+				for(int c=0; c<M; c++) flag[r][c]=row[c];
+			}
+			w=0;b=0;r=0;
+			ans=Integer.MAX_VALUE;
+			for(int i=0; i<N-2; i++) {
+				countCol(i,'W');
+				b=0;
+				for(int j=i+1; j<N-1; j++) {
+					countCol(j,'B');
+					r=0;
+					for(int k=j+1; k<N; k++) {
+						countCol(k,'R');
+					}
+					ans = Math.min(ans,w+b+r);
+				}
+			}
+			System.out.println("#"+tc+" "+ans);
 		}
 	}
-    
-    static int countCol(char[][] arr, char color, int row) {
-        int cnt=0;
-    	for(char c : arr[row]) {
-        	if(c!=color) cnt++;
-        }
-        return cnt;
-    }
+	
+	static void countCol(int row, char col) {
+		for(char c : flag[row]) {
+			if(c!=col) {
+				switch(col) {
+					case 'W': 
+						w++;
+						break;
+					case 'B':
+						b++;
+						break;
+					case 'R':
+						r++;
+						break;
+				} 
+			}
+		}
+	}
 }
