33c4e502969fce311053b281306a126c19afd9bb
Joeho <84345021+joeholee@users.noreply.github.com>
2024-08-25 20:38:32 +0900

    [D2] Title: 파리 퇴치, Time: 122 ms, Memory: 19,032 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -1,42 +1,36 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
- 
+
 class Solution
 {
-    public static void main(String args[]) throws Exception
-    {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        StringTokenizer st = new StringTokenizer(str);
-        int T = Integer.parseInt(st.nextToken());
-        for(int test_case = 1; test_case <= T; test_case++)
-        {
-            String[] input = br.readLine().split(" ");
-            int N = Integer.parseInt(input[0]);
-            int M = Integer.parseInt(input[1]);
-            int[][] arr = new int[N][N];
-            for(int i=0; i<N; i++) {
-                String[] tmp = br.readLine().split(" ");
-                for(int j=0; j<N; j++) arr[i][j]=Integer.parseInt(tmp[j]);
-            }
-            int ans=0;
-            for(int r=0; r<N; r++) {
-                for(int c=0; c<N; c++) {
-                    int sum=0;
-                    // 파리채
-                    for(int dr=0; dr<M; dr++) {
-                        for(int dc=0; dc<M; dc++) {
-                            int nr = r+dr;
-                            int nc = c+dc;
-                            if(nr<0||nr>=N||nc<0||nc>=N) continue;
-                            sum+=arr[nr][nc];
-                        }
-                    }
-                    if(sum>=ans) ans=sum;
-                }
-            }
-            System.out.println("#"+test_case+" "+ans);
-        }
-    }
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int N,M,ans;
+	static int[][] fly;
+	public static void main(String args[]) throws Exception
+	{
+		int T;
+		T=Integer.parseInt(br.readLine());
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			String[] sz = br.readLine().split(" ");
+			N = Integer.parseInt(sz[0]);
+			M = Integer.parseInt(sz[1]);
+			fly = new int[N][N];
+			for(int r=0; r<N; r++) {
+				String[] input = br.readLine().split(" ");
+				for(int c=0; c<N; c++) fly[r][c] = Integer.parseInt(input[c]);
+			}
+			for(int r=0; r<N-M+1; r++) {
+				for(int c=0; c<N-M+1; c++) {
+					int tmp=0;
+					for(int i=0; i<M; i++) {
+						for(int j=0; j<M; j++) tmp+=fly[r+i][c+j];
+						ans = Math.max(ans,tmp);
+					}
+				}
+			}
+			System.out.println("#"+test_case+" "+ans);
+            ans=0;
+		}
+	}
 }
