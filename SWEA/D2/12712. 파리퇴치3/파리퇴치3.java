d5c778e38beb0bbcbc8a92df42d7c87526776ae0
Joeho <84345021+joeholee@users.noreply.github.com>
2024-08-25 21:37:05 +0900

    [D2] Title: 파리퇴치3, Time: 116 ms, Memory: 19,308 KB -BaekjoonHub

--- SWEA/D2/12712. 파리퇴치3/파리퇴치3.java
+++ SWEA/D2/12712. 파리퇴치3/파리퇴치3.java
@@ -1,55 +1,53 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
- 
+
 class Solution
 {
-    public static void main(String args[]) throws Exception
-    {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        StringTokenizer st = new StringTokenizer(str);
-        int T = Integer.parseInt(st.nextToken());
-        int[] dr1 = {-1,1,0,0};
-        int[] dc1 = {0,0,-1,1};
-        int[] dr2 = {-1,-1,1,1};
-        int[] dc2 = {-1,1,1,-1};
-        for(int test_case = 1; test_case <= T; test_case++)
-        {
-            String[] input = br.readLine().split(" ");
-            int N = Integer.parseInt(input[0]);
-            int M = Integer.parseInt(input[1]);
-            int[][] arr = new int[N][N];
-            for(int i=0; i<N; i++) {
-                String[] tmp = br.readLine().split(" ");
-                for(int j=0; j<N; j++) arr[i][j] = Integer.parseInt(tmp[j]);
-            }
-            int ans=0;
-            for(int r=0; r<N; r++) {
-                for(int c=0; c<N; c++) {
-                    int sum=arr[r][c];
-                    for(int dir=0; dir<4; dir++) {
-                        for(int pow=1; pow<M; pow++) {
-                            int nr = r+pow*dr1[dir];
-                            int nc = c+pow*dc1[dir];
-                            if(nr<0||nr>=N||nc<0||nc>=N) continue;
-                            sum+=arr[nr][nc];
-                        }
-                    }
-                    if(sum>=ans) ans=sum;
-                    sum=arr[r][c];
-                    for(int dir=0; dir<4; dir++) {
-                        for(int pow=1; pow<M; pow++) {
-                            int nr = r+pow*dr2[dir];
-                            int nc = c+pow*dc2[dir];
-                            if(nr<0||nr>=N||nc<0||nc>=N) continue;
-                            sum+=arr[nr][nc];
-                        }
-                    }
-                    if(sum>=ans) ans=sum;
-                }
-            }
-            System.out.println("#"+test_case+" "+ans);
-        }
-    }
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int N,M,ans;
+	static int[][] fly;
+	static int[] dr = {-1,0,1,0,-1,-1,1,1};
+	static int[] dc = {0,1,0,-1,-1,1,1,-1};
+	public static void main(String args[]) throws Exception
+	{
+		int T;
+		T = Integer.parseInt(br.readLine());
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			String[] sz = br.readLine().split(" ");
+			N = Integer.parseInt(sz[0]);
+			M = Integer.parseInt(sz[1]);
+			fly = new int[N][N];
+			for(int r=0; r<N; r++) {
+				String[] input = br.readLine().split(" ");
+				for(int c=0; c<N; c++) fly[r][c]=Integer.parseInt(input[c]);
+			}
+			for(int r=0; r<N; r++) {
+				for(int c=0; c<N; c++) {
+					int tmp=fly[r][c];
+					for(int dir=0; dir<4; dir++) {
+						for(int k=1; k<M; k++) {
+							int nr = r+k*dr[dir];
+							int nc = c+k*dc[dir];
+							if(nr<0||nr>=N||nc<0||nc>=N) continue;
+							tmp+=fly[nr][nc];
+						}
+					}
+					ans = Math.max(ans,tmp);
+					tmp = fly[r][c];
+					for(int dir=4; dir<8; dir++) {
+						for(int k=1; k<M; k++) {
+							int nr = r+k*dr[dir];
+							int nc = c+k*dc[dir];
+							if(nr<0||nr>=N||nc<0||nc>=N) continue;
+							tmp+=fly[nr][nc];
+						}
+					}
+					ans = Math.max(ans,tmp);
+				}
+			}
+			System.out.println("#"+test_case+" "+ans);
+			ans=0;
+		}
+	}
 }
