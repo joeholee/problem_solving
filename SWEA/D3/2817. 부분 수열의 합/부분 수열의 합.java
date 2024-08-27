3262c1ff91aa9e3251c8f77ef454658ed287e100
Joeho <84345021+joeholee@users.noreply.github.com>
2024-08-27 17:48:09 +0900

    [D3] Title: 부분 수열의 합, Time: 2,700 ms, Memory: 23,788 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
@@ -1,43 +1,41 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
-class Solution
-{
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static int N,K,ans;
-	static int[] A,sel;
-	public static void main(String args[]) throws Exception
-	{
-		int T = Integer.parseInt(br.readLine());
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-			String[] sz = br.readLine().split(" ");
-			N = Integer.parseInt(sz[0]);
-			K = Integer.parseInt(sz[1]);
-			A = new int[N];
-			sel = new int[N];
-			String[] arr = br.readLine().split(" ");
-			for(int i=0; i<N; i++) A[i]=Integer.parseInt(arr[i]);
-			powerset(0);
-			System.out.println("#"+test_case+" "+ans);
-			ans=0;
+public class Solution {
+	public static void main(String[] args) {
+		//4:21 시작 4:40 종료 19분
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for (int t = 1; t <= T; t++) {
+			int N = sc.nextInt();
+			int K = sc.nextInt();
+			
+			int[] arr = new int[N];
+			
+			for (int i = 0; i < N; i++) {
+				arr[i] = sc.nextInt();
+			}
+			
+			int sum;
+			int cnt =0;
+			
+			// 부분집합의 개수, 첫번째 조합 => 1 << N
+			for (int i = 0; i < (1 << N); i++) {
+				sum=0;
+				for (int j = 0; j < N; j++) {
+					if((i & (1<<j)) != 0) {
+						sum += arr[j];
+					}
+					
+				
+				}
+				if(sum == K) {
+					cnt++;
+				}
+			}
+			System.out.println("#"+t+" "+cnt);
 		}
-	}
-	
-	static void powerset(int cnt) {
-		if(cnt==N) {
-			calcSum(sel);
-			return;
-		}
-		sel[cnt]=A[cnt];
-		powerset(cnt+1);
-		sel[cnt]=0;
-		powerset(cnt+1);
-	}
-	
-	static void calcSum(int[] sel) {
-		int tmp=0;
-		for(int s : sel) tmp+=s;
-		if(tmp==K) ans++;
+		
 	}
 }
