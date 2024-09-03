c4cea0596973d0c7b52d72ba2b05d8f6754c0690
Joeho <84345021+joeholee@users.noreply.github.com>
2024-09-03 18:20:51 +0900

    [Silver III] Title: N과 M (5), Time: 232 ms, Memory: 31312 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Main {
+	static int N,M;
+	static int[] arr,num;
+	static boolean[] vis;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		String[] sz = br.readLine().split(" ");
+		N = Integer.parseInt(sz[0]);
+		M = Integer.parseInt(sz[1]);
+		arr = new int[N];
+		num = new int[M];
+		vis = new boolean[N];
+		String[] nums = br.readLine().split(" ");
+		for(int i=0; i<N; i++) arr[i]=Integer.parseInt(nums[i]);
+		Arrays.sort(arr);
+		perm(0);
+		System.out.println(sb);
+	}
+	
+	static void perm(int cnt) {
+		if(cnt==M) {
+			for(int i : num) sb.append(i).append(" ");
+			sb.append("\n");
+			return;
+		}
+		for(int i=0; i<N; i++) {
+			if(!vis[i]) {
+				num[cnt]=arr[i];
+				vis[i]=true;
+				perm(cnt+1);
+				vis[i]=false;
+			}
+		}
+	}
+}
