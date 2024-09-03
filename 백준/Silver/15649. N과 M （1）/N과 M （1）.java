864b99262ff9603a4b2a67798fb4a64d8cb76fa8
Joeho <84345021+joeholee@users.noreply.github.com>
2024-09-03 17:24:30 +0900

    [Silver III] Title: N과 M (1), Time: 232 ms, Memory: 22252 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,42 +1,48 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N,M;
-    static StringTokenizer st;
-    static int[] arr;
-    static int[] num;
-    static boolean[] vis;
-    static StringBuilder sb = new StringBuilder();
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        arr = new int[N];
-        for(int i=0; i<arr.length; i++) arr[i]=i+1;
-        num = new int[M];
-        vis = new boolean[N];
-        perm(0);
-        System.out.println(sb);
-    }
-    
-    static void perm(int cnt) {
-        if(cnt==M) {
-            for(int i : num) sb.append(i+" ");
-            sb.append("\n");
-            return;
-        }
-        for(int i=0; i<arr.length; i++) {
-            if(!vis[i]) {
-                num[cnt]=arr[i];
-                vis[i]=true;
-                perm(cnt+1);
-                vis[i]=false;
-            }
-        }
-    }
+	static int[] arr;
+	static boolean[] visited;
+	static StringBuilder sb = new StringBuilder();
+	
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		arr = new int[M];
+		visited = new boolean[N+1];
+		
+		perm(N, M, 0);
+		System.out.println(sb);
+	}
+	
+	static void perm(int N, int M, int idx) {
+		//기저 조건
+		//M개 고른 배열의 요소 갯수는 M개, 인덱스는 M-1개 인덱스가 M개가 되면, return
+		if(idx == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(arr[i]).append(" ");	
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		//재귀 함수
+		//1~N까지를 순회해야 하므로 반복문 사용!, 중복을 허용하지 않으므로, visited 사용
+		for (int i = 1; i <= N; i++) {
+			if(!visited[i]) {
+				arr[idx]=i;
+				visited[i] = true;
+				perm(N, M, idx+1);
+				visited[i]=false;
+			}
+		}
+	}
+	
 }
