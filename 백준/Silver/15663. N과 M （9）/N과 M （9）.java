22fd255e1538e9e6600d4596f5f0b4928cce9f0f
Joeho <84345021+joeholee@users.noreply.github.com>
2024-09-09 15:35:30 +0900

    [Silver II] Title: N과 M (9), Time: 856 ms, Memory: 41452 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int[] arr,num;
+	static boolean[] vis;
+	static int N,M;
+	
+	public static void main(String[] args) throws IOException {
+		String[] sz = br.readLine().split(" ");
+		N = Integer.parseInt(sz[0]);
+		M = Integer.parseInt(sz[1]);
+		arr = new int[N];
+		num = new int[M];
+		vis = new boolean[N];
+		String[] nums = br.readLine().split(" ");
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(nums[i]);
+		}
+		Arrays.sort(arr);
+		perm(0);
+	}
+	
+	static void perm(int cnt) {
+		int prev=-1;
+		if(cnt==M) {
+			for(int i=0; i<M; i++) System.out.print(num[i]+" ");
+			System.out.println();
+			return;
+		}
+		for(int i=0; i<N; i++) {
+			if(!vis[i] && arr[i]!=prev) {
+				prev=arr[i];
+				num[cnt]=arr[i];
+				vis[i]=true;
+				perm(cnt+1);
+				vis[i]=false;
+			}
+		}
+	}
+}
