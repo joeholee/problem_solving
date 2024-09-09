be3aea03a6584c1f300206a6ca54da771c8338af
Joeho <84345021+joeholee@users.noreply.github.com>
2024-09-09 12:10:11 +0900

    [D1] Title: N줄덧셈, Time: 137 ms, Memory: 20,472 KB -BaekjoonHub

--- SWEA/D1/2025. N줄덧셈/N줄덧셈.java
+++ SWEA/D1/2025. N줄덧셈/N줄덧셈.java
@@ -1,10 +1,15 @@
 import java.util.Scanner;
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        System.out.print(N*(N+1)/2);
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		int sum = 0;
+		for (int i = 1; i < N+1; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
 	}
+
 }
