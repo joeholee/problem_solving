c6051286079f2ecb47e47446af75af0ff749d02c
Joeho <84345021+joeholee@users.noreply.github.com>
2024-09-09 11:55:01 +0900

    [D1] Title: 스탬프 찍기, Time: 122 ms, Memory: 18,428 KB -BaekjoonHub

--- SWEA/D1/2046. 스탬프 찍기/스탬프 찍기.java
+++ SWEA/D1/2046. 스탬프 찍기/스탬프 찍기.java
@@ -1,13 +1,20 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Solution {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		String ans = "";
-		for(int i=0; i<a; i++) ans += "#";
-		System.out.println(ans);
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 0; i < N; i++) {
+			sb.append("#");
+		}
+		
+		System.out.println(sb.toString());
+		
 	}
 
 }
