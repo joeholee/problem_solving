c17abeb83368772c974c315be215aed8f72b0285
Joeho <84345021+joeholee@users.noreply.github.com>
2024-09-12 11:49:52 +0900

    [D2] Title: 쉬운 거스름돈, Time: 106 ms, Memory: 16,080 KB -BaekjoonHub

--- SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.java
+++ SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.java
@@ -1,26 +1,37 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-
+import java.util.StringTokenizer;
+ 
 class Solution
 {
-	static int[] change = {50000,10000,5000,1000,500,100,50,10};
-	static int[] amount;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static int N;
+    static int[] change = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
+    static int[] dp;
     public static void main(String args[]) throws Exception
     {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-        amount = new int[8];
         for(int test_case = 1; test_case <= T; test_case++)
         {
-        	int N = Integer.parseInt(br.readLine());
-        	for(int i=0; i<8; i++) {
-        		amount[i]=N/change[i];
-        		N%=change[i];
-        	}
-        	System.out.println("#"+test_case);
-        	for(int i : amount) System.out.print(i+" ");
-        	System.out.println();
+            N = Integer.parseInt(br.readLine());
+            dp = new int[change.length];
+            for(int i=0; i<change.length; i++) {
+                if(change[i]>N) {
+                    dp[i]=0;
+                    continue;
+                }
+                int num = N/change[i];
+                for(int j=1; j<=num; j++) {
+                    N-=change[i];
+                    dp[i]++;
+                }
+            }
+            sb.append("#").append(test_case).append("\n");
+            for(int n : dp) sb.append(n).append(" ");
+            sb.append("\n");
         }
-        
+        System.out.println(sb);
     }
 }
