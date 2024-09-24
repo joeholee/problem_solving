697ef8563d54246d1cdceab525a2e942b4502edf
Joeho <84345021+joeholee@users.noreply.github.com>
2024-09-24 17:15:42 +0900

    [Silver I] Title: 안전 영역, Time: 268 ms, Memory: 53652 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,69 +1,61 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.StringTokenizer;
 import java.util.Queue;
 import java.util.LinkedList;
 import java.util.Arrays;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
 	static int N,max,ans;
+	static int[][] area;
+	static boolean[][] vis;
 	static int[] cur;
 	static int[] dr = {-1,0,1,0};
 	static int[] dc = {0,1,0,-1};
-	static int[][] height;
-	static boolean[][] vis;
 	static Queue<int[]> q = new LinkedList<>();
 	
 	public static void main(String[] args) throws IOException {
 		N = Integer.parseInt(br.readLine());
-		height = new int[N][N];
+		area = new int[N][N];
 		vis = new boolean[N][N];
+		max=Integer.MIN_VALUE;
 		for(int r=0; r<N; r++) {
-			String[] row = br.readLine().split(" ");
+			st = new StringTokenizer(br.readLine());
 			for(int c=0; c<N; c++) {
-				height[r][c]=Integer.parseInt(row[c]);
-				if(height[r][c]>max) max=height[r][c];
+				area[r][c]=Integer.parseInt(st.nextToken());
+				max = Math.max(max,area[r][c]);
 			}
 		}
-		for(int i=0; i<=max; i++) {
-			drain(i);
-			bfs();
-		}
+		ans=1;
+		for(int i=0; i<=max; i++) solve(i);
 		System.out.println(ans);
 	}
 	
-	static void drain(int lvl) {
+	static void solve(int height) {
+		int cnt=0;
 		for(int r=0; r<N; r++) {
 			for(int c=0; c<N; c++) {
-				if(height[r][c]<=lvl) height[r][c]=0;
-			}
-		}
-	}
-	
-	static void bfs() {
-		int tmp=0;
-		for(int r=0; r<N; r++) {
-			for(int c=0; c<N; c++) {
-				if(height[r][c]!=0 && !vis[r][c]) {
-					q.add(new int[] {r,c});
-					vis[r][c]=true;
-					while(!q.isEmpty()) {
-						cur = q.poll();
-						for(int dir=0; dir<4; dir++) {
-							int nr = cur[0]+dr[dir];
-							int nc = cur[1]+dc[dir];
-							if(nr<0||nr>=N||nc<0||nc>=N) continue;
-							if(vis[nr][nc]||height[nr][nc]==0) continue;
-							vis[nr][nc]=true;
-							q.add(new int[] {nr,nc});
-						}
+				if(area[r][c]<=height || vis[r][c]) continue;
+				vis[r][c]=true;
+				q.add(new int[] {r,c});
+				cnt++;
+				while(!q.isEmpty()) {
+					cur = q.poll();
+					for(int dir=0; dir<4; dir++) {
+						int nr = cur[0]+dr[dir];
+						int nc = cur[1]+dc[dir];
+						if(nr<0||nr>=N||nc<0||nc>=N) continue;
+						if(area[nr][nc]<=height || vis[nr][nc]) continue;
+						vis[nr][nc]=true;
+						q.add(new int[] {nr,nc});
 					}
-					tmp++;
 				}
 			}
 		}
-		ans = Integer.max(ans,tmp);
-		for(boolean[] v : vis) Arrays.fill(v,false);
+		ans = Math.max(ans, cnt);
+		for(boolean[] row : vis) Arrays.fill(row, false);
 	}
 }
