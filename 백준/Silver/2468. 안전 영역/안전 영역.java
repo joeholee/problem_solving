6c484dd5c3d138c0668928d2dde9c9dd9b35f021
Joeho <84345021+joeholee@users.noreply.github.com>
2024-09-27 15:58:48 +0900

    [Silver I] Title: 안전 영역, Time: 460 ms, Memory: 31596 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,61 +1,60 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
-import java.util.Queue;
-import java.util.LinkedList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
-	static BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	static int N,max,ans;
-	static int[][] area;
-	static boolean[][] vis;
-	static int[] cur;
-	static int[] dr = {-1,0,1,0};
-	static int[] dc = {0,1,0,-1};
-	static Queue<int[]> q = new LinkedList<>();
 	
-	public static void main(String[] args) throws IOException {
-		N = Integer.parseInt(br.readLine());
-		area = new int[N][N];
-		vis = new boolean[N][N];
-		max=Integer.MIN_VALUE;
-		for(int r=0; r<N; r++) {
-			st = new StringTokenizer(br.readLine());
-			for(int c=0; c<N; c++) {
-				area[r][c]=Integer.parseInt(st.nextToken());
-				max = Math.max(max,area[r][c]);
+	static int N;
+	
+	static int[][] map;
+	static boolean[][] visited;
+	
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		
+		map = new int[N][N];
+		
+		for (int r = 0; r < N; r++) {
+			for (int c = 0; c < N; c++) {
+				map[r][c] = sc.nextInt();
 			}
 		}
-		ans=1;
-		for(int i=0; i<=max; i++) solve(i);
-		System.out.println(ans);
-	}
-	
-	static void solve(int height) {
-		int cnt=0;
-		for(int r=0; r<N; r++) {
-			for(int c=0; c<N; c++) {
-				if(area[r][c]<=height || vis[r][c]) continue;
-				vis[r][c]=true;
-				q.add(new int[] {r,c});
-				cnt++;
-				while(!q.isEmpty()) {
-					cur = q.poll();
-					for(int dir=0; dir<4; dir++) {
-						int nr = cur[0]+dr[dir];
-						int nc = cur[1]+dc[dir];
-						if(nr<0||nr>=N||nc<0||nc>=N) continue;
-						if(area[nr][nc]<=height || vis[nr][nc]) continue;
-						vis[nr][nc]=true;
-						q.add(new int[] {nr,nc});
+		
+//		System.out.println(Arrays.deepToString(map));
+		int max = 0;
+		for(int depth=0; depth<=100; depth++) {
+			int cnt = 0;
+			visited = new boolean[N][N];
+			
+			for(int r=0; r<N; r++) {
+				for(int c=0; c<N; c++) {
+					if(map[r][c] > depth && !visited[r][c]) {
+						visited[r][c] = true;
+						dfs(r,c,depth);
+						cnt++; // 새로운 그래프를 찾았으므로.
+						
 					}
 				}
 			}
+			if(cnt > max) max = cnt;
 		}
-		ans = Math.max(ans, cnt);
-		for(boolean[] row : vis) Arrays.fill(row, false);
+		System.out.println(max);
+	}
+
+	private static void dfs(int r, int c, int depth) {
+		for(int d=0; d<4; d++) {
+			int nr = r+ dr[d];
+			int nc = c + dc[d];
+			if(nr < 0 || nr >=N || nc <0 || nc >= N) continue;
+			if(map[nr][nc]>depth && !visited[nr][nc]) {
+				visited[nr][nc] = true;
+				dfs(nr, nc, depth);
+			}
+		}
 	}
 }
