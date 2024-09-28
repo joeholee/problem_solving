8473458b370cec924f9336187de22f1f83f0dedc
Joeho <84345021+joeholee@users.noreply.github.com>
2024-09-28 22:02:05 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 108 ms, Memory: 20,120 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -1,48 +1,38 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.util.Queue;
-import java.util.LinkedList;
- 
+
 class Solution
 {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringBuilder sb = new StringBuilder();
-    static StringTokenizer st;
-    static int ans;
-    static int[] rate = new int[4];
-    static int[] plan = new int[12];
-    static Queue<Integer> q;
-     
-    public static void main(String args[]) throws Exception
-    {
-        int T = Integer.parseInt(br.readLine());
-         
-        for(int test_case = 1; test_case <= T; test_case++)
-        {
-            sb.append("#").append(test_case).append(" ");
-            st = new StringTokenizer(br.readLine());
-            for(int i=0; i<4; i++) rate[i]=Integer.parseInt(st.nextToken());
-            st = new StringTokenizer(br.readLine());
-            for(int i=0; i<12; i++) plan[i]=Integer.parseInt(st.nextToken());
-            ans = Integer.MAX_VALUE;
-            dfs(0,0);
-            sb.append(ans);
-            System.out.println(sb);
-            sb.setLength(0);
-        }
-    }
-     
-    static void dfs(int month, int total) {
-        if(month>=12) {
-            ans = Math.min(ans,total);
-            return;
-        }
-        int cur = plan[month];
-        int payDaily = cur*rate[0];
-        dfs(month+1,total+payDaily);
-        dfs(month+1,total+rate[1]);
-        if(month<=9) dfs(month+3,total+rate[2]);
-        dfs(month+12,total+rate[3]);
-    }
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+	static int[] price = new int[4]; // 1일 1달 3달 1
+	static int[] plan = new int[12];
+	static int[] dp;
+	
+	public static void main(String args[]) throws Exception
+	{
+		int T = Integer.parseInt(br.readLine());
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			st = new StringTokenizer(br.readLine());
+			for(int i=0; i<4; i++) price[i]=Integer.parseInt(st.nextToken());
+			st = new StringTokenizer(br.readLine());
+			for(int i=0; i<12; i++) plan[i]=Integer.parseInt(st.nextToken());
+			solve();
+			sb.append("#").append(test_case).append(" ").append(dp[11]).append("\n");
+		}
+		System.out.println(sb);
+	}
+	
+	static void solve() {
+		dp = new int[12];
+		dp[0]=Math.min(price[0]*plan[0], price[1]);
+		for(int i=1; i<12; i++) {
+			dp[i]=dp[i-1]+Math.min(price[0]*plan[i], price[1]);
+			if(i>=2) dp[i]=Math.min(dp[i], (i>=3 ? dp[i-3] : 0)+price[2]);
+		}
+		dp[11]=Math.min(dp[11], price[3]);
+	}
 }
