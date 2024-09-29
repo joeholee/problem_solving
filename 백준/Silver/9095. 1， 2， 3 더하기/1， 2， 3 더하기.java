c6ce88e608f54425291338d6a14d7ccae53af761
Joeho <84345021+joeholee@users.noreply.github.com>
2024-09-29 13:48:13 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 68 ms, Memory: 11452 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -4,27 +4,27 @@ import java.io.IOException;
 
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static int T,n,ans;
-	static int[] dp;
+	static StringBuilder sb = new StringBuilder();
+	static int[] dp = new int[11];
+	static int n;
 	
 	public static void main(String[] args) throws IOException {
-		T = Integer.parseInt(br.readLine());
-		for(int i=0; i<T; i++) {
+		int T = Integer.parseInt(br.readLine());
+		for(int t=0; t<T; t++) {
 			n = Integer.parseInt(br.readLine());
 			dp = new int[n+1];
-			if(n==1) ans=1;
-			else if(n==2) ans=2;
-			else if(n==3) ans=4;
-			else ans=solve(n);
-			System.out.println(ans);
+			solve();
 		}
+		System.out.println(sb);
 	}
 	
-	static int solve(int n) {
+	static void solve() {
+		dp[0]=0;
 		dp[1]=1;
-		dp[2]=2;
-		dp[3]=4;
-		for(int i=4; i<=n; i++) dp[i]=dp[i-3]+dp[i-2]+dp[i-1];
-		return dp[n];
+		if(n>=2) dp[2]=2;
+		if(n>=3) dp[3]=4;
+		if (n>=4)
+			for(int i=4; i<=n; i++) dp[i]=dp[i-3]+dp[i-2]+dp[i-1];
+		sb.append(dp[n]).append("\n");
 	}
 }
