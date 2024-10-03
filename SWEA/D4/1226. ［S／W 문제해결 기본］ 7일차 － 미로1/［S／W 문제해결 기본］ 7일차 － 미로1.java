50c75e151903be24a36e851849a988ae093759dd
Joeho <84345021+joeholee@users.noreply.github.com>
2024-10-03 17:53:10 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 104 ms, Memory: 18,828 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
@@ -10,37 +10,34 @@ class Solution
 	static StringBuilder sb = new StringBuilder();
 	static char[][] maze = new char[16][16];
 	static boolean[][] vis = new boolean[16][16];
-	static int[] cur;
-	static int[] fin = new int[2];
 	static int[] dr = {-1,0,1,0};
 	static int[] dc = {0,1,0,-1};
+	static int[] cur,end;
 	static Queue<int[]> q = new LinkedList<>();
 	
 	public static void main(String args[]) throws Exception
 	{
-		for(int test_case = 1; test_case <= 10; test_case++)
+		for(int t = 1; t <= 10; t++)
 		{
-			int t = Integer.parseInt(br.readLine());
-			for(boolean[] b : vis) Arrays.fill(b, false);
+			int tc = Integer.parseInt(br.readLine());
 			for(int r=0; r<16; r++) {
 				char[] row = br.readLine().toCharArray();
 				for(int c=0; c<16; c++) {
 					maze[r][c]=row[c];
-					if(maze[r][c]=='2') {
-						q.add(new int[] {r,c});
-						vis[r][c]=true;
-					}
-					if(maze[r][c]=='3') fin = new int[] {r,c};
+					if(maze[r][c]=='3') end = new int[] {r,c};
 				}
 			}
 			bfs();
-			sb.append("#").append(t).append(" ");
-			sb.append(vis[fin[0]][fin[1]] ? 1 : 0).append("\n");
+			sb.append("#").append(tc).append(" ").append(vis[end[0]][end[1]] ? 1 : 0).append("\n");
+			for(boolean[] row : vis) Arrays.fill(row, false);;
+			q.clear();
 		}
 		System.out.println(sb);
 	}
 	
 	static void bfs() {
+		vis[1][1]=true;
+		q.add(new int[] {1,1});
 		while(!q.isEmpty()) {
 			cur = q.poll();
 			for(int dir=0; dir<4; dir++) {
@@ -49,6 +46,7 @@ class Solution
 				if(nr<0||nr>=16||nc<0||nc>=16) continue;
 				if(vis[nr][nc]||maze[nr][nc]=='1') continue;
 				vis[nr][nc]=true;
+				if(nr==end[0]&&nc==end[1]) return;
 				q.add(new int[] {nr,nc});
 			}
 		}
