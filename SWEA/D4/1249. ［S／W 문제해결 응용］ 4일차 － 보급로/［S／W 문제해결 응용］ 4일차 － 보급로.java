2258189d44593bb1ba355888417a95dbbde0934b
Joeho <84345021+joeholee@users.noreply.github.com>
2024-10-05 23:28:08 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 148 ms, Memory: 24,372 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -1,54 +1,71 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Queue;
-import java.util.LinkedList;
-import java.util.Arrays;
+import java.util.PriorityQueue;
 
 class Solution
 {
+	static class Node implements Comparable<Node> {
+		int r,c,cost;
+		
+		Node(int r, int c, int cost) {
+			this.r=r;
+			this.c=c;
+			this.cost=cost;
+		}
+		
+		@Override
+		public int compareTo(Node other) {
+			return Integer.compare(this.cost, other.cost);
+		}
+	}
+	
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
 	static int N;
-	static int[][] map;
+	static char[][] map;
 	static int[][] dist;
 	static int[] dr = {-1,0,1,0};
 	static int[] dc = {0,1,0,-1};
-	static int[] cur;
-	static Queue<int[]> q = new LinkedList<>();
+	static PriorityQueue<Node> pq = new PriorityQueue<>();
 	
-	public static void main(String args[]) throws Exception
-	{
-		int C;
-		C = Integer.parseInt(br.readLine());
-		
-		for(int test_case = 1; test_case <= C; test_case++)
-		{
-			N = Integer.parseInt(br.readLine());
-			map = new int[N][N];
-			dist = new int[N][N];
-			for(int r=0; r<N; r++) {
-				String row = br.readLine();
-				for(int c=0; c<N; c++) map[r][c] = row.charAt(c)-'0';
-			}
-			for(int[] d : dist) Arrays.fill(d,Integer.MAX_VALUE);
-			bfs();
-			System.out.println("#"+test_case+" "+dist[N-1][N-1]);
-		}
-	}
-	
-	static void bfs() {
-		q.add(new int[] {0,0,0});
-		while(!q.isEmpty()) {
-			cur = q.poll();
-			for(int dir=0; dir<4; dir++) {
-				int nr = cur[0]+dr[dir];
-				int nc = cur[1]+dc[dir];
-				if(nr<0||nr>=N||nc<0||nc>=N) continue;
-				int nd = cur[2]+map[nr][nc];
-				if(dist[nr][nc]>nd) {
-					dist[nr][nc]=nd;
-					q.add(new int[] {nr,nc,nd});
-				}
-			}
-		}
-	}
+    public static void main(String args[]) throws Exception
+    {
+    	int T = Integer.parseInt(br.readLine());
+        for(int t = 1; t <= T; t++)
+        {
+        	sb.append("#").append(t).append(" ");
+        	N = Integer.parseInt(br.readLine());
+        	map = new char[N][N];
+        	dist = new int[N][N];
+        	for(int r=0; r<N; r++) {
+        		char[] row = br.readLine().toCharArray();
+        		for(int c=0; c<N; c++) {
+        			map[r][c]=row[c];
+        			dist[r][c]=Integer.MAX_VALUE;
+        		}
+        	}
+        	solve();
+        	sb.append(dist[N-1][N-1]).append("\n");
+        }
+        System.out.println(sb);
+    }
+    
+    static void solve() {
+    	dist[0][0]=0;
+    	pq.add(new Node(0,0,0));
+    	while(!pq.isEmpty()) {
+    		Node cur = pq.poll();
+    		for(int dir=0; dir<4; dir++) {
+    			int nr=cur.r+dr[dir];
+    			int nc=cur.c+dc[dir];
+    			if(nr>=0 && nr<N && nc>=0 && nc<N) {
+    				int ncost = cur.cost+(map[nr][nc]-'0');
+    				if(ncost<dist[nr][nc]) {
+    					dist[nr][nc]=ncost;
+    					pq.add(new Node(nr,nc,ncost));
+    				}
+    			}
+    		}
+    	}
+    }
 }
