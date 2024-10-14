2c74d2f2aea86eae2c03b3b4aff1ea44b182d04f
Joeho <84345021+joeholee@users.noreply.github.com>
2024-10-14 12:56:07 +0900

    [Silver I] Title: 스타트와 링크, Time: 280 ms, Memory: 15476 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -20,10 +20,8 @@ public class Main {
             }
         }
 
-        // 비트마스크를 활용해 조합을 생성하고 팀 구성을 나눔
         int totalStates = (1 << N);  // 2^N 상태 수
         for (int i = 1; i < totalStates; i++) {
-            // 팀 구성원의 수가 N/2가 아닌 경우는 패스
             if (Integer.bitCount(i) != N / 2) continue;
 
             calculateDifference(i);
@@ -32,17 +30,14 @@ public class Main {
         System.out.println(minDifference);
     }
 
-    // 비트마스크로 선택된 조합에 대해 차이를 계산
     public static void calculateDifference(int teamStartMask) {
         int teamStart = 0, teamLink = 0;
 
         for (int i = 0; i < N; i++) {
             for (int j = i + 1; j < N; j++) {
                 if ((teamStartMask & (1 << i)) != 0 && (teamStartMask & (1 << j)) != 0) {
-                    // i와 j가 모두 teamStart에 속하는 경우
                     teamStart += arr[i][j] + arr[j][i];
                 } else if ((teamStartMask & (1 << i)) == 0 && (teamStartMask & (1 << j)) == 0) {
-                    // i와 j가 모두 teamLink에 속하는 경우
                     teamLink += arr[i][j] + arr[j][i];
                 }
             }
