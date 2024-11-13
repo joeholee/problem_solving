9b21a287d6c8c47d1f2be78bb2310e6cf810437e
Joeho <84345021+joeholee@users.noreply.github.com>
2024-11-13 13:17:10 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 190 ms, Memory: 19,512 KB -BaekjoonHub

--- SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
@@ -4,38 +4,38 @@ import java.util.StringTokenizer;
 
 class Solution
 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
 	static int N,B,ans;
 	static int[] H;
-	static boolean[] vis;
 	
 	public static void main(String args[]) throws Exception
 	{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T;
-		T = Integer.parseInt(br.readLine());
+		int T = Integer.parseInt(br.readLine());
 		
 		for(int test_case = 1; test_case <= T; test_case++)
 		{
-            String[] sz = br.readLine().split(" ");
-            N = Integer.parseInt(sz[0]);
-            B = Integer.parseInt(sz[1]);
-            H = new int[N];
-            vis = new boolean[N];
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int i=0; i<N; i++) H[i]=Integer.parseInt(st.nextToken());
-            ans = Integer.MAX_VALUE;
-            subset(0,0);
-            System.out.println("#"+test_case+" "+(ans-B));
+			ans = Integer.MAX_VALUE;
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			B = Integer.parseInt(st.nextToken());
+			H = new int[N];
+			st = new StringTokenizer(br.readLine());
+			for(int i=0; i<N; i++) H[i]=Integer.parseInt(st.nextToken());
+			subset(0,0);
+			sb.append("#").append(test_case).append(" ").append(ans).append("\n");
 		}
+		System.out.println(sb);
 	}
 	
-	static void subset(int cnt, int sum) {
+	static void subset(int sum, int idx) {
 		if(sum>=B) {
-			ans = Math.min(ans,sum);
+			ans = Math.min(ans, sum-B);
 			return;
 		}
-		if(cnt==N) return;
-		subset(cnt+1,sum+H[cnt]);
-		subset(cnt+1,sum);
+		if(idx==N) return;
+		subset(sum+H[idx],idx+1);
+		subset(sum,idx+1);
 	}
 }
