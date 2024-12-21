4d2083598311e128b74ebb12ad05d639975e77c0
Joeho <84345021+joeholee@users.noreply.github.com>
2024-12-21 16:49:08 +0900

    [Bronze V] Title: 개수 세기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int N, v, arr[201] = {};
+    cin >> N;
+    while(N--) {
+        int t;
+        cin >> t;
+        arr[t+100]++;
+    }
+    cin >> v;
+    cout << arr[v+100];
+}
