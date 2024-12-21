052dbe8bbee87e12d73011d573835a53854a9143
Joeho <84345021+joeholee@users.noreply.github.com>
2024-12-21 16:24:32 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int A,B;
+    while(true) {
+        cin >> A >> B;
+        if(A==0 && B==0) break;
+        cout << A+B << '\n';
+    }
+}
