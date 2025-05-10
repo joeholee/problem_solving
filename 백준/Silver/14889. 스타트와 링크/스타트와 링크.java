36c3f16c9cfba2369f5ee7304db4f36ce0031420
Joeho <84345021+joeholee@users.noreply.github.com>
2025-05-10 13:07:04 +0900

    [Silver I] Title: 스타트와 링크, Time: 280 ms, Memory: 14440 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,49 +1,53 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N;
-    static int[][] arr;
-    static int minDifference = Integer.MAX_VALUE;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N][N];
-
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        int totalStates = (1 << N);  // 2^N 상태 수
-        for (int i = 1; i < totalStates; i++) {
-            if (Integer.bitCount(i) != N / 2) continue;
-
-            calculateDifference(i);
-        }
-
-        System.out.println(minDifference);
-    }
-
-    public static void calculateDifference(int teamStartMask) {
-        int teamStart = 0, teamLink = 0;
-
-        for (int i = 0; i < N; i++) {
-            for (int j = i + 1; j < N; j++) {
-                if ((teamStartMask & (1 << i)) != 0 && (teamStartMask & (1 << j)) != 0) {
-                    teamStart += arr[i][j] + arr[j][i];
-                } else if ((teamStartMask & (1 << i)) == 0 && (teamStartMask & (1 << j)) == 0) {
-                    teamLink += arr[i][j] + arr[j][i];
-                }
-            }
-        }
-
-        int difference = Math.abs(teamStart - teamLink);
-        minDifference = Math.min(minDifference, difference);
-    }
+	static int N;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static int[][] board;
+	static boolean[] vis;
+	static int ans = Integer.MAX_VALUE;
+	
+	public static void main(String[] args) throws IOException {
+		N = Integer.parseInt(br.readLine());
+		board = new int[N][N];
+		vis = new boolean[N];
+		for(int r=0; r<N; r++) {
+			st = new StringTokenizer(br.readLine());
+			for(int c=0; c<N; c++) board[r][c] = Integer.parseInt(st.nextToken());
+		}
+		comb(0,0);
+		System.out.println(ans);
+	}
+	
+	static void comb(int cnt, int idx) {
+		if(cnt==N/2) {
+			calc();
+			return;
+		}
+		for(int i=idx; i<N; i++) {
+			if(!vis[i]) {
+				vis[i] = true;
+				comb(cnt+1, i+1);
+				vis[i] = false;
+			}
+		}
+	}
+	
+	static void calc() {
+		int start=0, link = 0;
+		for(int r=0; r<N-1; r++) {
+			for(int c=r+1; c<N; c++) {
+				if(vis[r] && vis[c]) start+=board[r][c]+board[c][r];
+				if(!vis[r] && !vis[c]) link+=board[r][c]+board[c][r];
+			}
+		}
+		int tmp = Math.abs(start-link);
+		if(tmp==0) {
+			System.out.println(tmp);
+			System.exit(0);
+		}
+		ans = Math.min(ans, tmp);
+	}
 }
