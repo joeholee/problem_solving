a36bab825f1bbb4a8a00251ad27f0d6f2679d79d
Joeho <84345021+joeholee@users.noreply.github.com>
2025-07-28 19:02:49 +0900

    [Silver III] Title: 프린터 큐, Time: 92 ms, Memory: 12348 KB -BaekjoonHub

---
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static class Document{
+		int idx;
+		int priority;
+		
+		Document(int idx, int priority) {
+			this.idx=idx;
+			this.priority=priority;
+		}
+	}
+	
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+	static int T,N,M;
+	
+	public static void main(String[] args) throws IOException {
+		T = Integer.parseInt(br.readLine());
+		while(T-->0) {
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			Deque<Document> dq = new ArrayDeque<>();
+			PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+			st = new StringTokenizer(br.readLine());
+			for(int i=0; i<N; i++) {
+				int priority = Integer.parseInt(st.nextToken());
+				dq.offer(new Document(i,priority));
+				pq.offer(priority);
+			}
+			int cnt=0;
+			while(!dq.isEmpty()) {
+				Document doc = dq.poll();
+				if(doc.priority==pq.peek()) {
+					cnt++;
+					pq.poll();
+					if(doc.idx==M) {
+						sb.append(cnt).append('\n');
+						break;
+					}
+				}
+				else dq.offer(doc);
+			}
+		}
+		System.out.println(sb);
+	}
+}
