73205ddb4653882f09d8fbfdc900b7b4e7313ef6
Joeho <84345021+joeholee@users.noreply.github.com>
2025-07-29 21:57:26 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 536 ms, Memory: 56344 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static class Point implements Comparable<Point> {
+		int x;
+		int y;
+		
+		Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+		
+		@Override
+		public int compareTo(Point p) {
+			if(this.y==p.y) return this.x-p.x;
+			return this.y-p.y;
+		}
+	}
+	
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+	static int N;
+	static List<Point> l = new ArrayList<>();
+	
+	public static void main(String[] args) throws IOException {
+		N = Integer.parseInt(br.readLine());
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			l.add(new Point(x,y));
+		}
+		Collections.sort(l);
+		for(Point p : l) sb.append(p.x).append(' ').append(p.y).append('\n');
+		System.out.println(sb);
+	}
+}
