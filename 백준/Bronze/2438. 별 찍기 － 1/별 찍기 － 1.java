f11439ca18e1aa4df1efe6ea0b5dfd2c2a215607
Joeho <84345021+joeholee@users.noreply.github.com>
2025-07-30 08:40:47 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 64 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,11 +1,16 @@
-import java.util.Scanner;
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        for(int i=1; i<=N; i++) {
-            for(int j=1; j<=i; j++) System.out.print("*");
-            System.out.println();
-        }
-    }
-} 
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static int n,ans;
+	
+	public static void main(String[] args) throws IOException {
+		n = Integer.parseInt(br.readLine());
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=i; j++) sb.append('*');
+			sb.append('\n');
+		}
+		System.out.println(sb);
+	}
+}
