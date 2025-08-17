337bbbd142de7ee165a46389b7fe74726866874d
Joeho <84345021+joeholee@users.noreply.github.com>
2025-08-18 00:47:46 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 536 ms, Memory: 49356 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,40 +1,42 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+	
 	static class Edge implements Comparable<Edge> {
-		int from,to,val;
+		int from,to,weight;
 		
-		Edge(int from,int to,int val) {
+		Edge(int from, int to, int weight) {
 			this.from=from;
 			this.to=to;
-			this.val=val;
+			this.weight=weight;
 		}
 		
-		public int compareTo(Edge o) {
-			return this.val-o.val;
+		@Override
+		public int compareTo(Edge e) {
+			return Integer.compare(weight, e.weight);
 		}
 	}
 	
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
 	static int V,E,A,B,C,pick,ans;
 	static int[] p;
 	static Edge[] edges;
 	
 	public static void main(String[] args) throws IOException {
-		String[] sz = br.readLine().split(" ");
-		V = Integer.parseInt(sz[0]);
-		E = Integer.parseInt(sz[1]);
+		st = new StringTokenizer(br.readLine());
+		V = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
 		p = new int[V+1];
-		for(int i=1; i<=V; i++) p[i]=i; // makeSet
+		for(int i=1; i<=V; i++) p[i]=i;
 		edges = new Edge[E];
 		for(int i=0; i<E; i++) {
-			String[] input = br.readLine().split(" ");
-			A = Integer.parseInt(input[0]);
-			B = Integer.parseInt(input[1]);
-			C = Integer.parseInt(input[2]);
+			st = new StringTokenizer(br.readLine());
+			A = Integer.parseInt(st.nextToken());
+			B = Integer.parseInt(st.nextToken());
+			C = Integer.parseInt(st.nextToken());
 			edges[i] = new Edge(A,B,C);
 		}
 		Arrays.sort(edges);
@@ -43,21 +45,22 @@ public class Main {
 			int b = edges[i].to;
 			if(findSet(a)!=findSet(b)) {
 				union(a,b);
-				ans+=edges[i].val;
+				ans+=edges[i].weight;
 				pick++;
 			}
 			if(pick==V-1) break;
 		}
-		System.out.println(ans);
+		bw.write(ans+"");
+		bw.close();
+		br.close();
 	}
 	
-	static void union(int a,int b) {
-		p[findSet(b)]=p[a];
+	static void union(int a, int b) {
+		p[findSet(b)] = p[a];
 	}
 	
 	static int findSet(int x) {
 		if(p[x]!=x) p[x]=findSet(p[x]);
 		return p[x];
 	}
-	
 }
