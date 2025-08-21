7006fa524bf5ea4a112e18bb5d58ee3fd795d41d
Joeho <84345021+joeholee@users.noreply.github.com>
2025-08-21 23:10:39 +0900

    [Silver IV] Title: 주몽, Time: 136 ms, Memory: 15640 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -5,32 +5,34 @@ public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringTokenizer st;
-	static int[] arr,num;
+	static int[] arr;
 	static int N,M,ans;
 	
 	public static void main(String[] args) throws IOException {
 		N = Integer.parseInt(br.readLine());
 		M = Integer.parseInt(br.readLine());
 		arr = new int[N];
-		num = new int[2];
 		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++) arr[i]=Integer.parseInt(st.nextToken());
-		comb(0,0);
+		solve();
 		bw.write(ans+"");
 		bw.close();
 		br.close();
 	}
 	
-	static void comb(int idx, int cnt) {
-		if(cnt==2) {
-			int tmp=0;
-			for(int i : num) tmp+=i;
-			if(tmp==M) ans++;
-			return;
-		}
-		for(int i=idx; i<N; i++) {
-			num[cnt]=arr[i];
-			comb(i+1,cnt+1);
+	static void solve() {
+		Arrays.sort(arr);
+		int left=0;
+		int right=N-1;
+		while(left<right) {
+			int sum = arr[left]+arr[right];
+			if(sum==M) {
+				ans++;
+				left++;
+				right--;
+			}
+			else if(sum>M) right--;
+			else left++;
 		}
 	}
 }
