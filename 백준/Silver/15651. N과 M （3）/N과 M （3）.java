70aefdb3855293851fa497f92f6115b2a5cfc19e
Joeho <84345021+joeholee@users.noreply.github.com>
2025-08-21 22:40:08 +0900

    [Silver III] Title: N과 M (3), Time: 320 ms, Memory: 112056 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,36 +1,36 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N,M;
-    static int[] arr;
-    static int[] num;
-    static StringTokenizer st;
-    static StringBuilder sb;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        sb = new StringBuilder();
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        arr = new int[N];
-        num = new int[M];
-        for(int i=0; i<arr.length; i++) arr[i]=i+1;
-        perm(0);
-        System.out.print(sb);
-    }
-    
-    static void perm(int cnt) {
-        if(cnt==M) {
-            for(int i : num) sb.append(i+" ");
-            sb.append("\n");
-            return;
-        }
-        for(int i=0; i<arr.length; i++) {
-            num[cnt]=arr[i];
-            perm(cnt+1);
-        }
-    }
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+	static int N,M;
+	static int[] arr,num;
+	
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		num = new int[M];
+		for(int i=0; i<N; i++) arr[i] = i+1;
+		subset(0);
+		bw.write(sb.toString());
+		bw.close();
+		br.close();
+	}
+	
+	static void subset(int cnt) {
+		if(cnt==M) {
+			for(int i : num) sb.append(i).append(' ');
+			sb.append('\n');
+			return;
+		}
+		for(int i=0; i<N; i++) {
+			num[cnt]=arr[i];
+			subset(cnt+1);
+		}
+	}
 }
