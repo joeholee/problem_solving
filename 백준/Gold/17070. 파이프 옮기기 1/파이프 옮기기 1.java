2dfb14b35c3c8c453bab96ff7119a71ac7be8292
Joeho <84345021+joeholee@users.noreply.github.com>
2025-08-29 23:25:39 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 68 ms, Memory: 11824 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -5,39 +5,28 @@ public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringTokenizer st;
-	static int N,ans;
+	static int N;
 	static int[][] arr;
+	static int[][][] dp;
 	
 	public static void main(String[] args) throws IOException {
 		N = Integer.parseInt(br.readLine());
 		arr = new int[N][N];
+		dp = new int[N][N][3];
 		for(int r=0; r<N; r++) {
 			st = new StringTokenizer(br.readLine());
 			for(int c=0; c<N; c++) arr[r][c] = Integer.parseInt(st.nextToken());
 		}
-		dfs(0,1,0);
-		bw.write(ans+"");
+		dp[0][1][0]=1;
+		for(int r=0; r<N; r++) {
+			for(int c=2; c<N; c++) {
+				if(c-1>=0 && arr[r][c]==0) dp[r][c][0]=dp[r][c-1][0]+dp[r][c-1][2];
+				if(r-1>=0 && arr[r][c]==0) dp[r][c][1]=dp[r-1][c][1]+dp[r-1][c][2];
+				if(r-1>=0 && c-1>=0 && arr[r][c]==0 && arr[r-1][c]==0 && arr[r][c-1]==0) dp[r][c][2]=dp[r-1][c-1][0]+dp[r-1][c-1][1]+dp[r-1][c-1][2];
+			}
+		}
+		bw.write(dp[N-1][N-1][0]+dp[N-1][N-1][1]+dp[N-1][N-1][2]+"");
 		bw.close();
 		br.close();
-	}
-	
-	public static void dfs(int r, int c, int dir) {
-		if(r==N-1 && c==N-1) {
-			ans++;
-			return;
-		}
-		if(dir==0) {
-			if(c+1<N && arr[r][c+1]==0) dfs(r,c+1,0);
-			if(r+1<N && c+1<N && arr[r+1][c]==0 && arr[r][c+1]==0 && arr[r+1][c+1]==0) dfs(r+1,c+1,2);
-		}
-		else if(dir==1) {
-			if(r+1<N && arr[r+1][c]==0) dfs(r+1,c,1);
-			if(r+1<N && c+1<N && arr[r+1][c]==0 && arr[r][c+1]==0 && arr[r+1][c+1]==0) dfs(r+1,c+1,2);
-		}
-		else {
-			if(c+1<N && arr[r][c+1]==0) dfs(r,c+1,0);
-			if(r+1<N && arr[r+1][c]==0) dfs(r+1,c,1);
-			if(r+1<N && c+1<N && arr[r+1][c]==0 && arr[r][c+1]==0 && arr[r+1][c+1]==0) dfs(r+1,c+1,2);
-		}
 	}
 }
