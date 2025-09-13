434dceb1efdb4be7a4645a4397624df35c0eda1c
Joeho <84345021+joeholee@users.noreply.github.com>
2025-09-13 14:12:43 +0900

    [Silver III] Title: 달팽이, Time: 316 ms, Memory: 69576 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.java
+++ 백준/Silver/1913. 달팽이/달팽이.java
@@ -1,43 +1,42 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringBuilder sb = new StringBuilder();
-	static int N,dir,query,val;
-	static int[] cur, ans;
+	static int N,val,it,dir;
 	static int[][] arr;
 	static int[] dr = {1,0,-1,0};
 	static int[] dc = {0,1,0,-1};
+	static int[] ans = new int[2];
 	
 	public static void main(String[] args) throws IOException {
 		N = Integer.parseInt(br.readLine());
-		query = Integer.parseInt(br.readLine());
+		val = Integer.parseInt(br.readLine());
 		arr = new int[N][N];
-		arr[0][0] = N*N;
-		val = arr[0][0];
-		cur = new int[] {0,0};
-		ans = new int[2];
-		int num = N*N;
-		while(num-->0) {
-			arr[cur[0]][cur[1]]=val;
-			if(val==query) {
-				ans[0]=cur[0]+1;
-				ans[1]=cur[1]+1;
+		it = N*N;
+		int r=0,c=0;
+		while(it>0) {
+			arr[r][c]=it;
+			if(val==it) {
+				ans[0]=r+1;
+				ans[1]=c+1;
 			}
-			int nr = cur[0]+dr[dir];
-			int nc = cur[1]+dc[dir];
+			int nr = r+dr[dir];
+			int nc = c+dc[dir];
 			if(nr<0||nr>=N||nc<0||nc>=N||arr[nr][nc]!=0) dir = (dir+1)%4;
-			cur[0]+=dr[dir];
-			cur[1]+=dc[dir];
-			val--;
+			r+=dr[dir];
+			c+=dc[dir];
+			it--;
 		}
-		for(int r=0; r<N; r++) {
-			for(int c=0; c<N; c++) {
-				sb.append(arr[r][c]).append(' ');
-			}
+		for(int[] row : arr) {
+			for(int i : row) sb.append(i).append(' ');
 			sb.append('\n');
 		}
 		sb.append(ans[0]).append(' ').append(ans[1]);
-		System.out.println(sb);
+		bw.write(sb.toString());
+		bw.close();
+		br.close();
 	}
 }
