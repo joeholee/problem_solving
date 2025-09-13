683e6df17d7d966e3d3cba9fef9a1e26c74d1728
Joeho <84345021+joeholee@users.noreply.github.com>
2025-09-13 14:53:14 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 232 ms, Memory: 26264 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -1,34 +1,42 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int N,M,K,i,j,x,y;
-	static int[][] arr;
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
 	static StringBuilder sb = new StringBuilder();
+	static int N,M,K;
+	static int[][] arr,sum;
+	
 	public static void main(String[] args) throws IOException {
-		String[] sz = br.readLine().split(" ");
-		N = Integer.parseInt(sz[0]);
-		M = Integer.parseInt(sz[1]);
-		arr = new int[N+1][M+1];
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N][M];
+		for(int r=0; r<N; r++) {
+			st = new StringTokenizer(br.readLine());
+			for(int c=0; c<M; c++) arr[r][c] = Integer.parseInt(st.nextToken());
+		}
+		sum = new int[N+1][M+1];
 		for(int r=1; r<=N; r++) {
-			String[] row = br.readLine().split(" ");
-			for(int c=1; c<=M; c++) arr[r][c]=Integer.parseInt(row[c-1]);
+			for(int c=1; c<=M; c++) sum[r][c] = sum[r-1][c]+sum[r][c-1]-sum[r-1][c-1]+arr[r-1][c-1];
 		}
 		K = Integer.parseInt(br.readLine());
 		for(int k=0; k<K; k++) {
-			String[] pos = br.readLine().split(" ");
-			i = Integer.parseInt(pos[0]);
-			j = Integer.parseInt(pos[1]);
-			x = Integer.parseInt(pos[2]);
-			y = Integer.parseInt(pos[3]);
-			int tmp=0;
-			for(int r=i; r<=x; r++) {
-				for(int c=j; c<=y; c++) tmp+=arr[r][c];
-			}
-			sb.append(tmp).append("\n");
+			st = new StringTokenizer(br.readLine());
+			int i = Integer.parseInt(st.nextToken());
+			int j = Integer.parseInt(st.nextToken());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			sb.append(query(i,j,x,y)).append('\n');
 		}
-		System.out.println(sb);
+		bw.write(sb.toString());
+		bw.close();
+		br.close();
+	}
+	
+	static int query(int i, int j, int x, int y) {
+		return sum[x][y]-sum[x][j-1]-sum[i-1][y]+sum[i-1][j-1];
 	}
 }
