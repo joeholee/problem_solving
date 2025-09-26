ab2bf0b5ca9d98ccd76386478d641b5c4f048286
Joeho <84345021+joeholee@users.noreply.github.com>
2025-09-27 00:59:09 +0900

    [Gold III] Title: 줄 세우기, Time: 380 ms, Memory: 45188 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,49 +1,43 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
-import java.util.Queue;
-import java.util.LinkedList;
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringTokenizer st;
-	static int N,M;
-	static ArrayList<ArrayList<Integer>> adjList;
-	static int[] degree;
 	static StringBuilder sb = new StringBuilder();
-	static Queue<Integer> q = new LinkedList<>();
+	static int N,M;
+	static int[] degree;
+	static List<List<Integer>> adj = new ArrayList<>();
+	static Queue<Integer> q = new ArrayDeque<>();
 	
 	public static void main(String[] args) throws IOException {
 		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		adjList = new ArrayList<>();
-		for(int i=0; i<=N; i++) adjList.add(new ArrayList<>());
 		degree = new int[N+1];
+		for(int i=0; i<=N; i++) adj.add(new ArrayList<>());
 		for(int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int A = Integer.parseInt(st.nextToken());
-			int B = Integer.parseInt(st.nextToken());
-			adjList.get(A).add(B);
-			degree[B]++;
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			adj.get(a).add(b);
+			degree[b]++;
 		}
-		for(int i=1; i<=N; i++) {
-			if(degree[i]==0) q.add(i);
-		}
+		for(int i=1; i<=N; i++) if(degree[i]==0) q.offer(i);
 		solve();
+		bw.write(sb.toString());
+		bw.close();
+		br.close();
 	}
 	
 	static void solve() {
 		while(!q.isEmpty()) {
 			int cur = q.poll();
-			sb.append(cur).append(" ");
-			for(int nxt : adjList.get(cur)) {
-				degree[nxt]--;
-				if(degree[nxt]==0) q.add(nxt);
+			sb.append(cur).append(' ');
+			for(int nxt : adj.get(cur)) {
+				if(--degree[nxt]==0) q.offer(nxt);
 			}
 		}
-		System.out.println(sb);
 	}
 }
