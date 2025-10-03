cd6e71ffd0bee111323c128cfcf7dcc5e1ac226a
Joeho <84345021+joeholee@users.noreply.github.com>
2025-10-03 16:32:03 +0900

    [Silver II] Title: 연속합, Time: 176 ms, Memory: 21992 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -1,31 +1,28 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] numbers = new int[N];
-        int[] dp = new int[N]; // dp[i]는 i번째까지의 최대 연속합
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            numbers[i] = Integer.parseInt(st.nextToken());
-        }
-
-        // 초기값 설정
-        dp[0] = numbers[0];
-        int maxValue = dp[0];
-
-        // 동적 프로그래밍을 이용하여 최대 연속합 계산
-        for (int i = 1; i < N; i++) {
-            dp[i] = Math.max(dp[i - 1] + numbers[i], numbers[i]);
-            maxValue = Math.max(maxValue, dp[i]);
-        }
-
-        // 결과 출력
-        System.out.print(maxValue);
-    }
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	static int n;
+	static int[] arr;
+	static int[] dp;
+	
+	public static void main(String[] args) throws IOException {
+		n = Integer.parseInt(br.readLine());
+		arr = new int[n+1];
+		st = new StringTokenizer(br.readLine());
+		for(int i=1; i<=n; i++) arr[i] = Integer.parseInt(st.nextToken());
+		dp = new int[n+1];
+		dp[1] = arr[1];
+		int ans = dp[1];
+		for(int i=2; i<=n; i++) {
+			dp[i] = Math.max(arr[i], dp[i-1]+arr[i]);
+			ans = Math.max(ans, dp[i]);
+		}
+		bw.write(ans+"");
+		bw.close();
+		br.close();
+	}
 }
