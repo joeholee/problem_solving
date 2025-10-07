796bf148b086c7d655d5ba65bd374c286908e26e
Joeho <84345021+joeholee@users.noreply.github.com>
2025-10-07 13:12:33 +0900

    [level 2] Title: 피보나치 수, Time: 4.45 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int n) {
+        int[] fib = new int[n+1];
+        fib[1]=1;
+        for(int i=2; i<=n; i++) fib[i]=(fib[i-1]+fib[i-2])%1234567;
+        int answer = fib[n];
+        return answer;
+    }
+}
