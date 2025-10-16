1b5752c09e91d5c931e09e7172e17ae17fc18bbc
Joeho <84345021+joeholee@users.noreply.github.com>
2025-10-16 13:55:32 +0900

    [Gold V] Title: 배열 돌리기 2, Time: 488 ms, Memory: 34224 KB -BaekjoonHub

---
+++ 백준/Gold/16927. 배열 돌리기 2/배열 돌리기 2.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+	static int N,M,R;
+	static int[][] A;
+	
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		R = Integer.parseInt(st.nextToken());
+		A = new int[N][M];
+		for(int r=0; r<N; r++) {
+			st = new StringTokenizer(br.readLine());
+			for(int c=0; c<M; c++) A[r][c] = Integer.parseInt(st.nextToken());
+		}
+		rotate();
+		for(int r=0; r<N; r++) {
+			for(int c=0; c<M; c++) sb.append(A[r][c]).append(' ');
+			sb.append('\n');
+		}
+		bw.write(sb.toString());
+		bw.close();
+		br.close();
+	}
+	
+	static void rotate() {
+		int cnt = Math.min(N, M)/2;
+		for(int i=0; i<cnt; i++) {
+			int width = M-2*i;
+			int height = N-2*i;
+			int num = R%(2*(width+height-2));
+			rotateLayer(i,num);
+		}
+	}
+	
+	static void rotateLayer(int layer, int num) {
+		for(int i=0; i<num; i++) {
+			int startRow = layer;
+			int endRow = N-layer-1;
+			int startCol = layer;
+			int endCol = M-layer-1;
+			int tmp = A[startRow][startCol];
+			for(int c=startCol; c<endCol; c++) A[startRow][c] = A[startRow][c+1];
+			for(int r=startRow; r<endRow; r++) A[r][endCol] = A[r+1][endCol];
+			for(int c=endCol; c>startCol; c--) A[endRow][c] = A[endRow][c-1];
+			for(int r=endRow; r>startRow; r--) A[r][startCol] = A[r-1][startCol];
+			A[startRow+1][startCol] = tmp;
+		}
+	}
+}
