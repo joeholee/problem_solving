09e61f09a109d9373e0d1b88c995e588231088ff
Joeho <84345021+joeholee@users.noreply.github.com>
2025-10-24 13:35:25 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 68 ms, Memory: 11608 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
@@ -5,35 +5,24 @@ public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringBuilder sb = new StringBuilder();
-	static int n,cnt;
+	static int n;
 	
 	public static void main(String[] args) throws IOException {
 		n = Integer.parseInt(br.readLine());
-		fib(n);
-		sb.append(cnt).append(' ');
-		cnt=0;
-		fibonacci(n);
-		sb.append(cnt);
+		sb.append(fib(n)).append(' ').append(fibonacci(n));
 		bw.write(sb.toString());
 		bw.close();
 		br.close();
 	}
 	
 	static int fib(int n) {
-		if(n==1||n==2) {
-			cnt++;
-			return 1;
-		}
-		else return fib(n-1)+fib(n-2);		
+		int[] f = new int[n+1];
+		f[1]=1; f[2]=1;
+		for(int i=3; i<=n; i++) f[i]=f[i-1]+f[i-2];
+		return f[n];
 	}
 	
 	static int fibonacci(int n) {
-		int[] f = new int[n+1];
-		f[1]=1; f[2]=1;
-		for(int i=3; i<=n; i++) {
-			cnt++;
-			f[i]=f[i-1]+f[i-2];
-		}
-		return f[n];
+		return n-2;
 	}
 }
