e17d66e9b18c528d9c0061450d593554e59163ce
Joeho <84345021+joeholee@users.noreply.github.com>
2025-10-24 13:31:57 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 360 ms, Memory: 11568 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
@@ -1,35 +1,39 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int[] dp;
-    static int[] memo;  // 메모이제이션 배열 추가
-    static int nomarlCnt;
-    static int dpCnt;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int N = Integer.parseInt(br.readLine());
-        dp = new int[N + 1];
-        memo = new int[N + 1];  // 메모이제이션 배열 초기화
-        nomarlCnt = 1;
-        dpCnt = 0;
-        System.out.print(fibonacciDp(N) + " " + dpCnt);
-    }
-
-
-
-    // DP를 활용한 피보나치 계산
-    static int fibonacciDp(int n) {
-        dp[1] = dp[2] = 1;  // 초기 값 설정
-
-        for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-            dpCnt++;
-        }
-        return dp[n];
-    }
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringBuilder sb = new StringBuilder();
+	static int n,cnt;
+	
+	public static void main(String[] args) throws IOException {
+		n = Integer.parseInt(br.readLine());
+		fib(n);
+		sb.append(cnt).append(' ');
+		cnt=0;
+		fibonacci(n);
+		sb.append(cnt);
+		bw.write(sb.toString());
+		bw.close();
+		br.close();
+	}
+	
+	static int fib(int n) {
+		if(n==1||n==2) {
+			cnt++;
+			return 1;
+		}
+		else return fib(n-1)+fib(n-2);		
+	}
+	
+	static int fibonacci(int n) {
+		int[] f = new int[n+1];
+		f[1]=1; f[2]=1;
+		for(int i=3; i<=n; i++) {
+			cnt++;
+			f[i]=f[i-1]+f[i-2];
+		}
+		return f[n];
+	}
 }
