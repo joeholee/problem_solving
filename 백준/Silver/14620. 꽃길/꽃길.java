faa61fe6c834273583f9489feb701a520f21e2ad
Joeho <84345021+joeholee@users.noreply.github.com>
2025-10-25 20:09:52 +0900

    [Silver II] Title: 꽃길, Time: 76 ms, Memory: 12556 KB -BaekjoonHub

--- 백준/Silver/14620. 꽃길/꽃길.java
+++ 백준/Silver/14620. 꽃길/꽃길.java
@@ -7,7 +7,7 @@ public class Main {
 	static StringTokenizer st;
 	static int N,ans=987654321;
 	static int[][] G;
-	static int[] pick = new int[3];
+	static boolean[][] vis;
 	static final int[] dr = {-1,0,1,0};
 	static final int[] dc = {0,1,0,-1};
 	
@@ -18,45 +18,57 @@ public class Main {
 			st = new StringTokenizer(br.readLine());
 			for(int c=0; c<N; c++) G[r][c] = Integer.parseInt(st.nextToken());
 		}
-		comb(0,0);
+		vis = new boolean[N][N];
+		comb(0,0,0);
 		bw.write(ans+"");
 		bw.close();
 		br.close();
 	}
 	
-	static void comb(int cnt, int idx) {
+	static void comb(int cnt, int idx, int sum) {
+		if(sum>=ans) return;
 		if(cnt==3) {
-			boolean[][] vis = new boolean[N][N];
-			for(int i : pick) {
-				int curR = i/N, curC = i%N;
-				if(vis[curR][curC]) return;
-				vis[curR][curC]=true;
-				for(int dir=0; dir<4; dir++) {
-					int nr = curR+dr[dir];
-					int nc = curC+dc[dir];
-					if(vis[nr][nc]) return;
-					vis[nr][nc]=true;
-				}
-			}
-			int tmp = 0;
-			for(int i : pick) {
-				int curR = i/N, curC = i%N;
-				tmp+=G[i/N][i%N];
-				for(int dir=0; dir<4; dir++) {
-					int nr = curR+dr[dir];
-					int nc = curC+dc[dir];
-					tmp+=G[nr][nc];
-				}
-			}
-			ans = Math.min(ans, tmp);
+			ans = Math.min(ans, sum);
 			return;
 		}
 		for(int i=idx; i<N*N; i++) {
 			int r = i/N, c = i%N;
-			if(r>0&&r<N-1&&c>0&&c<N-1) {
-				pick[cnt]=i;
-				comb(cnt+1,i+1);
-			}
+			if(r==0||r==N-1||c==0||c==N-1) continue;
+			if(!canVisit(r,c)) continue;
+			int tmp = calcSum(r,c);
+			check(r,c,true);
+			comb(cnt+1,i+1,sum+tmp);
+			check(r,c,false);
+		}
+	}
+	
+	static boolean canVisit(int r, int c) {
+		if(vis[r][c]) return false;
+		for(int dir=0; dir<4; dir++) {
+			int nr = r+dr[dir];
+			int nc = c+dc[dir];
+			if(vis[nr][nc]) return false;
+		}
+		return true;
+	}
+	
+	static int calcSum(int r, int c) {
+		int sum = 0;
+		sum+=G[r][c];
+		for(int dir=0; dir<4; dir++) {
+			int nr = r+dr[dir];
+			int nc = c+dc[dir];
+			sum+=G[nr][nc];
+		}
+		return sum;
+	}
+	
+	static void check(int r, int c, boolean flag) {
+		vis[r][c] = flag;
+		for(int dir=0; dir<4; dir++) {
+			int nr = r+dr[dir];
+			int nc = c+dc[dir];
+			vis[nr][nc] = flag;
 		}
 	}
 }
