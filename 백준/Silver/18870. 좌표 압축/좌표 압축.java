9e9717adbec932bc81be3b4fc344121289383d51
Joeho <84345021+joeholee@users.noreply.github.com>
2025-12-04 23:42:57 +0900

    [Silver II] Title: 좌표 압축, Time: 1408 ms, Memory: 249576 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -2,26 +2,33 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int N,idx;
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringTokenizer st;
-	static int[] arr,sor;
-	static Map<Integer, Integer> m = new HashMap<>();
 	static StringBuilder sb = new StringBuilder();
+	static int N;
+	static int[] X,tmp;
+	static List<Integer> li = new ArrayList<>();
 	
 	public static void main(String[] args) throws IOException {
 		N = Integer.parseInt(br.readLine());
-		arr = new int[N];
+		X = new int[N];
+		tmp = new int[N];
 		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) arr[i] = Integer.parseInt(st.nextToken());
-		sor = arr.clone();
-		Arrays.sort(sor);
-		for(int i : sor) {
-			if(!m.containsKey(i)) m.put(i, idx++);
+		for(int i=0; i<N; i++) {
+			X[i] = Integer.parseInt(st.nextToken());
+			tmp[i] = X[i];
 		}
-		for(int i : arr) {
-			sb.append(m.get(i)).append(' ');
+		Arrays.sort(tmp);
+		for(int i=0; i<N; i++) {
+			if(i==0||tmp[i-1]!=tmp[i]) li.add(tmp[i]);
 		}
-		System.out.println(sb);
+		Collections.sort(li);
+		for(int i=0; i<N; i++) {
+			sb.append(Collections.binarySearch(li, X[i])).append(' ');
+		}
+		bw.write(sb.toString());
+		bw.close();
+		br.close();
 	}
 }
