0932eb295143500442f76a8f9e7fe27193fd44b9
Joeho <84345021+joeholee@users.noreply.github.com>
2026-02-08 01:44:55 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,23 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-int arr[9], sum;
-int main(void) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    for(int i=0; i<9; i++) {
-        cin >> arr[i];
-        sum += arr[i];
-    }
-    sort(arr, arr+9);
-    for(int a=0; a<8; a++) {
-        for(int b=1; b<9; b++) {
-            if(sum-arr[a]-arr[b] == 100) {
-                for(int c=0; c<9; c++) {
-                    if(c==a || c==b) continue;
-                    cout << arr[c] << '\n';
-                }
-                return 0;
-            }
-        }
-    }
+int arr[9];
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    for(int i=0; i<9; i++) cin >> arr[i];
+    sort(arr,arr+9);
+    do {
+        int sum=0;
+        for(int i=0; i<7; i++) sum+=arr[i];
+        if(sum==100) break;
+    } while(next_permutation(arr,arr+9));
+    for(int i=0; i<7; i++) cout << arr[i] << '\n';
+    return 0;
 }
