3d494f9d42adf0345a22e4ba424509b0bfc280bb
Joeho <84345021+joeholee@users.noreply.github.com>
2026-02-12 15:40:00 +0900

    [Silver IV] Title: 주몽, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.cc
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -2,19 +2,6 @@
 using namespace std;
 int N,M,ret;
 vector<int> a;
-void comb(int idx, vector<int> &v) {
-    if(v.size()==2) {
-        int tmp=0;
-        for(int i : v) tmp+=i;
-        if(tmp==M) ret++;
-        return;
-    }
-    for(int i=idx; i<a.size(); i++) {
-        v.push_back(a[i]);
-        comb(i+1,v);
-        v.pop_back();
-    }
-}
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
     cin >> N;
@@ -25,8 +12,16 @@ int main() {
         a.push_back(x);
     }
     sort(a.begin(),a.end());
-    vector<int> tmp;
-    comb(0,tmp);
+    int l=0,r=N-1;
+    while(l<r) {
+        int sum=a[l]+a[r];
+        if(sum==M) {
+            ret++;
+            l++;
+            r--;
+        }
+        else if(sum>M) r--;
+        else l++;
+    }
     cout << ret << '\n';
-    return 0;
 }
