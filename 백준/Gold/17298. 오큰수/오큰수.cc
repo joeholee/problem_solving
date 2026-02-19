6bc7ad7d92cd9d73f6ba388cd9e7f3a8b34ddb40
Joeho <84345021+joeholee@users.noreply.github.com>
2026-02-20 03:52:19 +0900

    [Gold IV] Title: 오큰수, Time: 212 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.cc
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -1,17 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-int N, a[1000000], ans[1000000];
-int main(void) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+int N,A[1000001];
+vector<int> ret;
+stack<int> st;
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
     cin >> N;
-    for(int i=0; i<N; i++) cin >> a[i];
-    stack<int> S;
-    for(int i=N-1; i>=0; i--) {
-        while(!S.empty() && S.top() <= a[i]) S.pop();
-        if(S.empty()) ans[i] = -1;
-        else ans[i] = S.top();
-        S.push(a[i]);
+    for(int i=0; i<N; i++) {
+        cin >> A[i];
     }
-    for(int i=0; i<N; i++) cout << ans[i] << ' ';
+    ret = vector<int>(N,-1);
+    for(int i=0; i<N; i++) {
+        while(!st.empty()&&A[st.top()]<A[i]) {
+            ret[st.top()]=A[i];
+            st.pop();
+        }
+        st.push(i);
+    }
+    for(int i : ret) cout << i << ' ';
+    return 0;
 }
