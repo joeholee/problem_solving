a1f3df43715f801e6e78236f202d8ce3e6247290
Joeho <84345021+joeholee@users.noreply.github.com>
2026-03-06 09:15:27 +0900

    [Gold I] Title: 동전 뒤집기, Time: 292 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.cc
+++ 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.cc
@@ -1,28 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-int N,ret=1e9;
-char a[21][21];
+int N,a[21],ret=1e9;
 string s;
+void solve(int idx) {
+    if(idx==N) {
+        int tmp=0;
+        for(int i=1; i<=(1<<(N-1)); i*=2) {
+            int cnt=0;
+            for(int j=0; j<N; j++) if(a[j]&i) cnt++;
+            tmp+=min(cnt,N-cnt);
+        }
+        ret=min(ret,tmp);
+        return;
+    }
+    solve(idx+1);
+    a[idx]=~a[idx];
+    solve(idx+1);
+}
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
     cin >> N;
-    for(int r=0; r<N; r++) {
+    for(int i=0; i<N; i++) {
         cin >> s;
-        for(int c=0; c<N; c++) a[r][c]=s[c];
-    }
-    for(int mask=0; mask<(1<<N); mask++) {
-        int sum=0;
-        for(int c=0; c<N; c++) {
-            int cnt=0;
-            for(int r=0; r<N; r++) {
-                char cur = a[r][c];
-                if(mask&(1<<r)) cur = (cur=='T' ? 'H' : 'T');
-                if(cur=='T') cnt++;
-            }
-            sum+=min(cnt,N-cnt);
+        int val=1;
+        for(int j=0; j<N; j++) {
+            if(s[j]=='T') a[i]|=val;
+            val*=2;
         }
-        ret=min(ret,sum);
     }
+    solve(0);
     cout << ret << '\n';
     return 0;
 }
