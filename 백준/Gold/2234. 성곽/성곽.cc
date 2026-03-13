5c4a61643677824660c2ec65a7d6b21fcabd1cca
Joeho <84345021+joeholee@users.noreply.github.com>
2026-03-13 16:16:44 +0900

    [Gold III] Title: 성곽, Time: 0 ms, Memory: 2052 KB -BaekjoonHub

--- 백준/Gold/2234. 성곽/성곽.cc
+++ 백준/Gold/2234. 성곽/성곽.cc
@@ -1,56 +1,20 @@
 #include <bits/stdc++.h>
-using namespace std;
-#define R first
-#define C second
-int N,M,a[51][51],vis[51][51],ret1,ret2,ret3;
-queue<pair<int,int>> q;
-const int dr[] = {0,-1,0,1};
-const int dc[] = {-1,0,1,0};
-void bfs(int r, int c) {
-    int tmp=1;
-    vis[r][c]=1;
-    q.push({r,c});
-    while(!q.empty()) {
-        auto cur = q.front(); q.pop();
-        for(int dir=0; dir<4; dir++) {
-            int nr=cur.R+dr[dir];
-            int nc=cur.C+dc[dir];
-            if(nr<0||nr>=M||nc<0||nc>=N) continue;
-            if(vis[nr][nc]) continue;
-            if(a[cur.R][cur.C]&(1<<dir)) continue;
-            tmp++;
-            vis[nr][nc]=1;
-            q.push({nr,nc});
+using namespace std;   
+const int dr[] = {0,-1,0,1}; 
+const int dc[] = {-1,0,1,0}; 
+int vis[51][51],a[51][51],comp[2504],N,M,cnt,mx,big;
+int dfs(int r, int c, int cnt) {
+    if(vis[r][c]) return 0;
+    vis[r][c]=cnt;
+    int ret=1;
+    for(int dir=0; dir<4; dir++) {
+        if(!(a[r][c]&(1<<dir))) {
+            int nr=r+dr[dir];
+            int nc=c+dc[dir];
+            ret+=dfs(nr,nc,cnt);
         }
     }
-    ret2=max(ret2,tmp);
-}
-void solve() {
-    for(int r=0; r<M; r++) {
-        for(int c=0; c<N; c++) {
-            if(!vis[r][c]) {
-                bfs(r,c);
-                ret1++;
-            }
-        }
-    }
-}
-void wall() {
-    for(int r=0; r<M; r++) {
-        for(int c=0; c<N; c++) {
-            for(int dir=0; dir<4; dir++) {
-                int nr=r+dr[dir];
-                int nc=c+dc[dir];
-                if(nr<0||nr>=M||nc<0||nc>=N) continue;
-                if(a[r][c]&(1<<dir)) {
-                    memset(vis,0,sizeof(vis));
-                    a[r][c]&=~(1<<dir);
-                    solve();
-                    a[r][c]|=(1<<dir);
-                }
-            }
-        }
-    }
+    return ret;
 }
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
@@ -58,9 +22,29 @@ int main() {
     for(int r=0; r<M; r++) {
         for(int c=0; c<N; c++) cin >> a[r][c];
     }
-    solve();
-    cout << ret1 << '\n' << ret2 << '\n';
-    wall();
-    cout << ret2;
+    for(int r=0; r<M; r++) {
+        for(int c=0; c<N; c++) {
+            if(!vis[r][c]) {
+                cnt++;
+                comp[cnt]=dfs(r,c,cnt);
+                mx=max(mx,comp[cnt]);
+            }
+        }
+    }
+    for(int r=0; r<M; r++) {
+        for(int c=0; c<N; c++) {
+            if(r+1<M) {
+                int a=vis[r+1][c];
+                int b=vis[r][c];
+                if(a!=b) big=max(big,comp[a]+comp[b]);
+            }
+            if(c+1<N) {
+                int a=vis[r][c+1];
+                int b=vis[r][c];
+                if(a!=b) big=max(big,comp[a]+comp[b]);
+            }
+        }
+    }
+    cout << cnt << '\n' << mx << '\n' << big;
     return 0;
 }
