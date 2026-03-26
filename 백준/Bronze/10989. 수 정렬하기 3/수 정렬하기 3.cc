2102c62b36e83413f947ea1a2f82404d2ec15e71
Joeho <84345021+joeholee@users.noreply.github.com>
2026-03-27 04:57:52 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1524 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -1,17 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-int N,x;
-map<int,int> m;
+int N,x,a[10001];
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
     cin >> N;
     for(int i=0; i<N; i++) {
         cin >> x;
-        if(m[x]==0) m[x]=1;
-        else m[x]++; 
+        a[x]++;
     }
-    for(auto it : m) {
-        for(int i=0; i<it.second; i++) cout << it.first << '\n';
+    for(int i=1; i<=10000; i++) {
+        if(a[i]) {
+            for(int j=0; j<a[i]; j++) cout << i << '\n';
+        }
     }
     return 0;
 }
