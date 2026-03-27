f0c1c47d04630589e3592077e71985ec5245b085
Joeho <84345021+joeholee@users.noreply.github.com>
2026-03-27 18:45:13 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 504 ms, Memory: 43828 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,17 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-int N,M,x,cnt[20000001];
+int N,M,x;
+unordered_map<int,int> m;
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
     cin >> N;
     for(int i=0; i<N; i++) {
         cin >> x;
-        cnt[x+10000000]++;
+        m[x]++;
     }
     cin >> M;
     for(int i=0; i<M; i++) {
         cin >> x;
-        cout << cnt[x+10000000] << ' ';
+        cout << m[x] << ' ';
     }
     return 0;
 }
