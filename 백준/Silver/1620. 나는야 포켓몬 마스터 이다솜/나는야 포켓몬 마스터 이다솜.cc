23474e3bda9daa67196f24a20eb5a8bf5340e822
Joeho <84345021+joeholee@users.noreply.github.com>
2026-03-27 17:41:50 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 228 ms, Memory: 23944 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -1,20 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 int N,M;
-string tmp;
-map<string,int> m1;
-map<int,string> m2;
+map<int,string> m1;
+map<string,int> m2;
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
     cin >> N >> M;
     for(int i=1; i<=N; i++) {
-        cin >> tmp;
-        m1.insert({tmp,i});
-        m2.insert({i,tmp});
+        string s;
+        cin >> s;
+        m1.insert({i,s});
+        m2.insert({s,i});
     }
-    for(int i=1; i<=M; i++) {
-        cin >> tmp;
-        if(atoi(tmp.c_str())) cout << m2[atoi(tmp.c_str())] << '\n';
-        else cout << m1[tmp] << '\n';
+    for(int i=0; i<M; i++) {
+        string query;
+        cin >> query;
+        if(atoi(query.c_str())) cout << m1[stoi(query)] << '\n';
+        else cout << m2[query] << '\n';
     }
+    return 0;
 }
