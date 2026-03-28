b10898683f142baeca677431dc6da2734e63b05c
Joeho <84345021+joeholee@users.noreply.github.com>
2026-03-28 14:44:58 +0900

    [Silver IV] Title: 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.cc
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -1,24 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 int T;
-string S;
-bool solve(string S) {
-    stack<char> st;
-    for(char c : S) {
-        if(c=='(') st.push(c);
-        else {
-            if(st.empty()) return false;
-            st.pop();
-        }
-    }
-    return st.empty();
-}
+string s;
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
     cin >> T;
     for(int t=0; t<T; t++) {
-        cin >> S;
-        cout << ((solve(S)) ? "YES\n" : "NO\n");
+        cin >> s;
+        stack<char> st;
+        for(char c : s) {
+            if(c=='(') st.push(c);
+            else {
+                if(st.size()&&st.top()=='(') st.pop();
+                else st.push(c);
+            }
+        }
+        cout << (st.empty() ? "YES" : "NO") << '\n';
     }
     return 0;
 }
