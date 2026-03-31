a951074735533a13fdd551178b5d4ef3dda30c76
Joeho <84345021+joeholee@users.noreply.github.com>
2026-03-31 11:13:09 +0900

    [Silver IV] Title: 소가 길을 건너간 이유 3, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/14469. 소가 길을 건너간 이유 3/소가 길을 건너간 이유 3.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N,a,t,ret;
+vector<pair<int,int>> v;
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> N;
+    for(int i=0; i<N; i++) {
+        cin >> a >> t;
+        v.push_back({a,t});
+    }
+    sort(v.begin(),v.end());
+    ret=v[0].first;
+    for(int i=0; i<N; i++) {
+        if(ret<=v[i].first) ret=v[i].first;
+        ret+=v[i].second;
+    }
+    cout << ret;
+    return 0;
+}
