0bc5eb177f257c19f77928e590aa26e57d60ca33
Joeho <84345021+joeholee@users.noreply.github.com>
2026-04-03 05:29:00 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.cc
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.cc
@@ -1,47 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 int N,K,ret;
+const int INF=101;
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
     cin >> N >> K;
     vector<int> v(K);
-    for(int i=0; i<K; i++) cin >> v[i];
-    vector<int> vv;
+    int last=-1;
+    for(int i=0; i<K; i++) {
+        cin >> v[i];
+        last=max(last,v[i]);
+    }
+    vector<queue<int>> pos(last+1);
+    for(int i=0; i<K; i++) pos[v[i]].push(i);
+    vector<int> next(last+1,INF);
+    vector<bool> use(last+1,false);
+    int plug=0;
+    priority_queue<pair<int,int>> pq;
     for(int i=0; i<K; i++) {
         int cur=v[i];
-        bool use=false;
-        for(int x : vv) {
-            if(x==cur) {
-                use=1;
-                break;
-            }
-        }
-        if(use) continue;
-        if(vv.size()<N) {
-            vv.push_back(cur);
+        pos[cur].pop();
+        int curnxt=pos[cur].empty()? INF : pos[cur].front();
+        if(use[cur]) {
+            next[cur]=curnxt;
+            pq.push({curnxt,cur});
             continue;
         }
-        int target=-1;
-        int far=-1;
-        for(int j=0; j<vv.size(); j++) {
-            int dev=vv[j];
-            int nxt=-1;
-            for(int k=i+1; k<K; k++) {
-                if(v[k]==dev) {
-                    nxt=k;
-                    break;
-                }
-            }
-            if(nxt==-1) {
-                target=j;
-                break;
-            }
-            if(nxt>far) {
-                far=nxt;
-                target=j;
-            }
+        if(plug<N) {
+            plug++;
+            use[cur]=1;
+            next[cur]=curnxt;
+            pq.push({curnxt,cur});
+            continue;
         }
-        vv[target]=cur;
+        while(pq.size()) {
+            int idx=pq.top().first;
+            int dev=pq.top().second;
+            if(!use[dev]) {
+                pq.pop();
+                continue;
+            }
+            if(next[dev]!=idx) {
+                pq.pop();
+                continue;
+            }
+            use[dev]=0;
+            pq.pop();
+            break;
+        }
+        use[cur]=1;
+        next[cur]=curnxt;
+        pq.push({next[cur],cur});
         ret++;
     }
     cout << ret;
