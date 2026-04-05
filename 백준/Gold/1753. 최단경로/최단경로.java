dd5062395e674fb127a77f5fd7b22387b03d0245
Joeho <84345021+joeholee@users.noreply.github.com>
2026-04-05 11:43:38 +0900

    [Gold IV] Title: 최단경로, Time: 624 ms, Memory: 117520 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -2,14 +2,13 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	static StringBuilder sb = new StringBuilder();
 	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
 	static int V,E,K,u,v,w,dist[];
-	static final int INF = 987654321;
 	static List<List<Edge>> adj = new ArrayList<>();
+	static final int INF = 987654321;
 	
 	static class Edge implements Comparable<Edge> {
 		int v,w;
@@ -24,15 +23,12 @@ public class Main {
 			return Integer.compare(w, e.w);
 		}
 	}
-	
 	public static void main(String[] args) throws IOException {
 		st = new StringTokenizer(br.readLine());
 		V = Integer.parseInt(st.nextToken());
 		E = Integer.parseInt(st.nextToken());
-		dist = new int[V+1];
-		Arrays.fill(dist, INF);
-		for(int i=0; i<=V; i++) adj.add(new ArrayList<>());
 		K = Integer.parseInt(br.readLine());
+		for(int i=0; i<=V; i++) adj.add(new ArrayList<>());
 		for(int i=0; i<E; i++) {
 			st = new StringTokenizer(br.readLine());
 			u = Integer.parseInt(st.nextToken());
@@ -40,6 +36,9 @@ public class Main {
 			w = Integer.parseInt(st.nextToken());
 			adj.get(u).add(new Edge(v,w));
 		}
+		dist = new int[V+1];
+		Arrays.fill(dist,INF);
+		dist[K]=0;
 		dijkstra(K);
 		for(int i=1; i<=V; i++) sb.append(dist[i]==INF ? "INF" : dist[i]).append('\n');
 		bw.write(sb.toString());
@@ -50,14 +49,13 @@ public class Main {
 	static void dijkstra(int K) {
 		PriorityQueue<Edge> pq = new PriorityQueue<>();
 		pq.offer(new Edge(K,0));
-		dist[K]=0;
 		while(!pq.isEmpty()) {
 			Edge cur = pq.poll();
 			if(dist[cur.v]!=cur.w) continue;
-			for(Edge nxt : adj.get(cur.v)) {
-				if(dist[nxt.v]>dist[cur.v]+nxt.w) {
-					dist[nxt.v]=dist[cur.v]+nxt.w;
-					pq.offer(new Edge(nxt.v,dist[nxt.v]));
+			for(Edge e : adj.get(cur.v)) {
+				if(dist[e.v]>dist[cur.v]+e.w) {
+					dist[e.v]=dist[cur.v]+e.w;
+					pq.offer(new Edge(e.v,dist[e.v]));
 				}
 			}
 		}
