e8641337d2b3940dd7d19be13cd64db25d3e5bca
Joeho <84345021+joeholee@users.noreply.github.com>
2026-04-05 11:19:31 +0900

    [Gold IV] Title: 강의실 배정, Time: 680 ms, Memory: 75804 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -5,39 +5,26 @@ public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringTokenizer st;
-	static int N;
-	static Lecture[] l;
-	
-	static class Lecture implements Comparable<Lecture> {
-		int S,T;
-		
-		Lecture(int S, int T) {
-			this.S = S;
-			this.T = T;
-		}
-		
-		@Override
-		public int compareTo(Lecture l) {
-			if(S==l.S) return Integer.compare(T, l.T);
-			return Integer.compare(S, l.S);
-		}
-	}
-	
+	static int N,arr[][];
+	static PriorityQueue<Integer> pq = new PriorityQueue<>();
 	public static void main(String[] args) throws IOException {
 		N = Integer.parseInt(br.readLine());
-		l = new Lecture[N];
+		arr = new int[N][2];
 		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken());
-			int t = Integer.parseInt(st.nextToken());
-			l[i] = new Lecture(s,t);
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
 		}
-		Arrays.sort(l);
-		PriorityQueue<Integer> pq = new PriorityQueue<>();
-		pq.offer(l[0].T);
-		for(int i=1; i<N; i++) {
-			if(pq.peek()<=l[i].S) pq.poll();
-			pq.offer(l[i].T);
+		Arrays.sort(arr, (o1,o2) -> {
+			if(o1[0]==o2[0]) return Integer.compare(o1[1], o2[1]);
+			else return Integer.compare(o1[0],o2[0]);
+		});
+		for(int i=0; i<N; i++) {
+			if(pq.isEmpty()) pq.offer(arr[i][1]);
+			else {
+				if(pq.peek()<=arr[i][0]) pq.poll();
+				pq.offer(arr[i][1]);
+			}
 		}
 		bw.write(pq.size()+"");
 		bw.close();
