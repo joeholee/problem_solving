8cf13cc5226cba9099e6199ced00a10f59cbb902
Joeho <84345021+joeholee@users.noreply.github.com>
2026-04-05 11:59:13 +0900

    [Gold V] Title: 최소비용 구하기, Time: 456 ms, Memory: 51524 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -2,29 +2,26 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	static int N,M,from,to,cost,A,B,dist[];
+	static List<List<Edge>> adj = new ArrayList<>();
+	static final int INF = 1098765432;
 	
-	static class Bus implements Comparable<Bus> {
+	static class Edge implements Comparable<Edge> {
 		int to,cost;
 		
-		Bus(int to, int cost) {
+		Edge(int to, int cost) {
 			this.to = to;
 			this.cost = cost;
 		}
 		
 		@Override
-		public int compareTo(Bus b) {
-			return Integer.compare(cost, b.cost);
+		public int compareTo(Edge e) {
+			return Integer.compare(cost, e.cost);
 		}
 	}
-	
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	static StringTokenizer st;
-	static int N,M,start,end;
-	static int[] dist;
-	static List<List<Bus>> adj = new ArrayList<>();
-	static PriorityQueue<Bus> pq = new PriorityQueue<>();
-	static final int INF = 987654321;
 	
 	public static void main(String[] args) throws IOException {
 		N = Integer.parseInt(br.readLine());
@@ -32,32 +29,33 @@ public class Main {
 		for(int i=0; i<=N; i++) adj.add(new ArrayList<>());
 		for(int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			int cost = Integer.parseInt(st.nextToken());
-			adj.get(from).add(new Bus(to,cost));
+			from = Integer.parseInt(st.nextToken());
+			to = Integer.parseInt(st.nextToken());
+			cost = Integer.parseInt(st.nextToken());
+			adj.get(from).add(new Edge(to,cost));
 		}
 		st = new StringTokenizer(br.readLine());
-		start = Integer.parseInt(st.nextToken());
-		end = Integer.parseInt(st.nextToken());
+		A = Integer.parseInt(st.nextToken());
+		B = Integer.parseInt(st.nextToken());
 		dist = new int[N+1];
 		Arrays.fill(dist, INF);
-		dijkstra(start);
-		bw.write(dist[end]+"");
+		dijkstra(A);
+		bw.write(dist[B]+"");
 		bw.close();
 		br.close();
 	}
 	
-	static void dijkstra(int start) {
-		dist[start]=0;
-		pq.add(new Bus(start,0));
+	static void dijkstra(int A) {
+		dist[A]=0;
+		PriorityQueue<Edge> pq = new PriorityQueue<>();
+		pq.offer(new Edge(A,0));
 		while(!pq.isEmpty()) {
-			Bus cur = pq.poll();
+			Edge cur = pq.poll();
 			if(dist[cur.to]!=cur.cost) continue;
-			for(Bus b : adj.get(cur.to)) {
-				if(dist[b.to]>dist[cur.to]+b.cost) {
-					dist[b.to]=dist[cur.to]+b.cost;
-					pq.add(new Bus(b.to,dist[b.to]));
+			for(Edge e : adj.get(cur.to)) {
+				if(dist[e.to]>dist[cur.to]+e.cost) {
+					dist[e.to]=dist[cur.to]+e.cost;
+					pq.offer(new Edge(e.to,dist[e.to]));
 				}
 			}
 		}
