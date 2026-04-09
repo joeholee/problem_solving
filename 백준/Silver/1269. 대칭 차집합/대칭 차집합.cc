56da0ae130b4148ecfb5f0ae2b70fdb5d07794c0
Joeho <84345021+joeholee@users.noreply.github.com>
2026-04-10 03:13:21 +0900

    [Silver IV] Title: 대칭 차집합, Time: 188 ms, Memory: 20768 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
@@ -1,21 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-int a,b,x,ret;
-unordered_map<int,int> m;
+int a,b,num;
+set<int> s;
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
     cin >> a >> b;
     for(int i=0; i<a; i++) {
-        cin >> x;
-        m[x]++;
+        cin >> num;
+        s.insert(num);
     }
     for(int i=0; i<b; i++) {
-        cin >> x;
-        m[x]++;
+        cin >> num;
+        if(s.count(num)) s.erase(num);
+        else s.insert(num);
     }
-    for(auto it : m) {
-        if(it.second==1) ret++;
-    }
-    cout << ret;
+    cout << s.size();
     return 0;
 }
