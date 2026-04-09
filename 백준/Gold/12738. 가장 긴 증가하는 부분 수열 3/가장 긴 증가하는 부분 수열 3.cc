8049f2b7685784935181e69371e0e37502602cfc
Joeho <84345021+joeholee@users.noreply.github.com>
2026-04-10 01:50:22 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 180 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N,ret,num,lis[1000001];
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> N;
+    for(int i=0; i<N; i++) {
+        cin >> num;
+        auto pos = lower_bound(lis,lis+ret,num);
+        if(*pos==0) ret++;
+        *pos=num;
+    }
+    cout << ret;
+    return 0;
+}
