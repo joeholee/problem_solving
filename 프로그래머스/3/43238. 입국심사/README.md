a42bd2bffacde1c08a3548f248446719a7245b0d
Joeho <84345021+joeholee@users.noreply.github.com>
2026-06-07 03:19:52 +0900

    [level 3] Title: 입국심사, Time: 36.07 ms, Memory: 70.1 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=java) 
 
 ### 성능 요약
 
-메모리: 6.79 MB, 시간: 49.45 ms
+메모리: 70.1 MB, 시간: 36.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 29일 05:40:40
+2026년 06월 07일 03:19:48
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,18 +1,18 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public long solution(int n, int[] times) {
         long answer = Long.MAX_VALUE;
         Arrays.sort(times);
-        long left=0, right=(long) n*times[times.length-1];
-        while(left<=right) {
-            long mid = (left+right)/2;
+        long st=0, en=(long) n*times[times.length-1];
+        while(st<=en) {
+            long mid = (st+en)/2;
             long sum = 0;
-            for(int i : times) sum+=mid/i;
+            for(int i : times) sum += (mid/i);
             if(sum>=n) {
-                right=mid-1;
-                answer = Math.min(answer, mid);
-            } else left=mid+1;
+                en=mid-1;
+                answer = Math.min(answer,mid);
+            } else st=mid+1;
         }
         return answer;
     }
